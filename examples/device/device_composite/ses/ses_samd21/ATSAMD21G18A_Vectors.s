155edc7b006d24f15b7a74e9e323c9d34f0651e7
hathach <thach@tinyusb.org>
2018-11-21 12:36:28 +0700

    fix ses irq vector

--- examples/device/device_composite/ses/ses_samd21/ATSAMD21G18A_Vectors.s
+++ examples/device/device_composite/ses/ses_samd21/ATSAMD21G18A_Vectors.s
@@ -60,206 +60,206 @@ Dummy_Handler:
 
 #if defined(__OPTIMIZATION_SMALL)
 
-  .weak PM_IRQHandler
-  .thumb_set PM_IRQHandler,Dummy_Handler
+  .weak PM_Handler
+  .thumb_set PM_Handler,Dummy_Handler
 
-  .weak SYSCTRL_IRQHandler
-  .thumb_set SYSCTRL_IRQHandler,Dummy_Handler
+  .weak SYSCTRL_Handler
+  .thumb_set SYSCTRL_Handler,Dummy_Handler
 
-  .weak WDT_IRQHandler
-  .thumb_set WDT_IRQHandler,Dummy_Handler
+  .weak WDT_Handler
+  .thumb_set WDT_Handler,Dummy_Handler
 
-  .weak RTC_IRQHandler
-  .thumb_set RTC_IRQHandler,Dummy_Handler
+  .weak RTC_Handler
+  .thumb_set RTC_Handler,Dummy_Handler
 
-  .weak EIC_IRQHandler
-  .thumb_set EIC_IRQHandler,Dummy_Handler
+  .weak EIC_Handler
+  .thumb_set EIC_Handler,Dummy_Handler
 
-  .weak NVMCTRL_IRQHandler
-  .thumb_set NVMCTRL_IRQHandler,Dummy_Handler
+  .weak NVMCTRL_Handler
+  .thumb_set NVMCTRL_Handler,Dummy_Handler
 
-  .weak DMAC_IRQHandler
-  .thumb_set DMAC_IRQHandler,Dummy_Handler
+  .weak DMAC_Handler
+  .thumb_set DMAC_Handler,Dummy_Handler
 
-  .weak USB_IRQHandler
-  .thumb_set USB_IRQHandler,Dummy_Handler
+  .weak USB_Handler
+  .thumb_set USB_Handler,Dummy_Handler
 
-  .weak EVSYS_IRQHandler
-  .thumb_set EVSYS_IRQHandler,Dummy_Handler
+  .weak EVSYS_Handler
+  .thumb_set EVSYS_Handler,Dummy_Handler
 
-  .weak SERCOM0_IRQHandler
-  .thumb_set SERCOM0_IRQHandler,Dummy_Handler
+  .weak SERCOM0_Handler
+  .thumb_set SERCOM0_Handler,Dummy_Handler
 
-  .weak SERCOM1_IRQHandler
-  .thumb_set SERCOM1_IRQHandler,Dummy_Handler
+  .weak SERCOM1_Handler
+  .thumb_set SERCOM1_Handler,Dummy_Handler
 
-  .weak SERCOM2_IRQHandler
-  .thumb_set SERCOM2_IRQHandler,Dummy_Handler
+  .weak SERCOM2_Handler
+  .thumb_set SERCOM2_Handler,Dummy_Handler
 
-  .weak SERCOM3_IRQHandler
-  .thumb_set SERCOM3_IRQHandler,Dummy_Handler
+  .weak SERCOM3_Handler
+  .thumb_set SERCOM3_Handler,Dummy_Handler
 
-  .weak SERCOM4_IRQHandler
-  .thumb_set SERCOM4_IRQHandler,Dummy_Handler
+  .weak SERCOM4_Handler
+  .thumb_set SERCOM4_Handler,Dummy_Handler
 
-  .weak SERCOM5_IRQHandler
-  .thumb_set SERCOM5_IRQHandler,Dummy_Handler
+  .weak SERCOM5_Handler
+  .thumb_set SERCOM5_Handler,Dummy_Handler
 
-  .weak TCC0_IRQHandler
-  .thumb_set TCC0_IRQHandler,Dummy_Handler
+  .weak TCC0_Handler
+  .thumb_set TCC0_Handler,Dummy_Handler
 
-  .weak TCC1_IRQHandler
-  .thumb_set TCC1_IRQHandler,Dummy_Handler
+  .weak TCC1_Handler
+  .thumb_set TCC1_Handler,Dummy_Handler
 
-  .weak TCC2_IRQHandler
-  .thumb_set TCC2_IRQHandler,Dummy_Handler
+  .weak TCC2_Handler
+  .thumb_set TCC2_Handler,Dummy_Handler
 
-  .weak TC3_IRQHandler
-  .thumb_set TC3_IRQHandler,Dummy_Handler
+  .weak TC3_Handler
+  .thumb_set TC3_Handler,Dummy_Handler
 
-  .weak TC4_IRQHandler
-  .thumb_set TC4_IRQHandler,Dummy_Handler
+  .weak TC4_Handler
+  .thumb_set TC4_Handler,Dummy_Handler
 
-  .weak TC5_IRQHandler
-  .thumb_set TC5_IRQHandler,Dummy_Handler
+  .weak TC5_Handler
+  .thumb_set TC5_Handler,Dummy_Handler
 
-  .weak ADC_IRQHandler
-  .thumb_set ADC_IRQHandler,Dummy_Handler
+  .weak ADC_Handler
+  .thumb_set ADC_Handler,Dummy_Handler
 
-  .weak AC_IRQHandler
-  .thumb_set AC_IRQHandler,Dummy_Handler
+  .weak AC_Handler
+  .thumb_set AC_Handler,Dummy_Handler
 
-  .weak DAC_IRQHandler
-  .thumb_set DAC_IRQHandler,Dummy_Handler
+  .weak DAC_Handler
+  .thumb_set DAC_Handler,Dummy_Handler
 
-  .weak I2S_IRQHandler
-  .thumb_set I2S_IRQHandler,Dummy_Handler
+  .weak I2S_Handler
+  .thumb_set I2S_Handler,Dummy_Handler
 
 #else
 
   .thumb_func
-  .weak PM_IRQHandler
-PM_IRQHandler:
+  .weak PM_Handler
+PM_Handler:
   b .
 
   .thumb_func
-  .weak SYSCTRL_IRQHandler
-SYSCTRL_IRQHandler:
+  .weak SYSCTRL_Handler
+SYSCTRL_Handler:
   b .
 
   .thumb_func
-  .weak WDT_IRQHandler
-WDT_IRQHandler:
+  .weak WDT_Handler
+WDT_Handler:
   b .
 
   .thumb_func
-  .weak RTC_IRQHandler
-RTC_IRQHandler:
+  .weak RTC_Handler
+RTC_Handler:
   b .
 
   .thumb_func
-  .weak EIC_IRQHandler
-EIC_IRQHandler:
+  .weak EIC_Handler
+EIC_Handler:
   b .
 
   .thumb_func
-  .weak NVMCTRL_IRQHandler
-NVMCTRL_IRQHandler:
+  .weak NVMCTRL_Handler
+NVMCTRL_Handler:
   b .
 
   .thumb_func
-  .weak DMAC_IRQHandler
-DMAC_IRQHandler:
+  .weak DMAC_Handler
+DMAC_Handler:
   b .
 
   .thumb_func
-  .weak USB_IRQHandler
-USB_IRQHandler:
+  .weak USB_Handler
+USB_Handler:
   b .
 
   .thumb_func
-  .weak EVSYS_IRQHandler
-EVSYS_IRQHandler:
+  .weak EVSYS_Handler
+EVSYS_Handler:
   b .
 
   .thumb_func
-  .weak SERCOM0_IRQHandler
-SERCOM0_IRQHandler:
+  .weak SERCOM0_Handler
+SERCOM0_Handler:
   b .
 
   .thumb_func
-  .weak SERCOM1_IRQHandler
-SERCOM1_IRQHandler:
+  .weak SERCOM1_Handler
+SERCOM1_Handler:
   b .
 
   .thumb_func
-  .weak SERCOM2_IRQHandler
-SERCOM2_IRQHandler:
+  .weak SERCOM2_Handler
+SERCOM2_Handler:
   b .
 
   .thumb_func
-  .weak SERCOM3_IRQHandler
-SERCOM3_IRQHandler:
+  .weak SERCOM3_Handler
+SERCOM3_Handler:
   b .
 
   .thumb_func
-  .weak SERCOM4_IRQHandler
-SERCOM4_IRQHandler:
+  .weak SERCOM4_Handler
+SERCOM4_Handler:
   b .
 
   .thumb_func
-  .weak SERCOM5_IRQHandler
-SERCOM5_IRQHandler:
+  .weak SERCOM5_Handler
+SERCOM5_Handler:
   b .
 
   .thumb_func
-  .weak TCC0_IRQHandler
-TCC0_IRQHandler:
+  .weak TCC0_Handler
+TCC0_Handler:
   b .
 
   .thumb_func
-  .weak TCC1_IRQHandler
-TCC1_IRQHandler:
+  .weak TCC1_Handler
+TCC1_Handler:
   b .
 
   .thumb_func
-  .weak TCC2_IRQHandler
-TCC2_IRQHandler:
+  .weak TCC2_Handler
+TCC2_Handler:
   b .
 
   .thumb_func
-  .weak TC3_IRQHandler
-TC3_IRQHandler:
+  .weak TC3_Handler
+TC3_Handler:
   b .
 
   .thumb_func
-  .weak TC4_IRQHandler
-TC4_IRQHandler:
+  .weak TC4_Handler
+TC4_Handler:
   b .
 
   .thumb_func
-  .weak TC5_IRQHandler
-TC5_IRQHandler:
+  .weak TC5_Handler
+TC5_Handler:
   b .
 
   .thumb_func
-  .weak ADC_IRQHandler
-ADC_IRQHandler:
+  .weak ADC_Handler
+ADC_Handler:
   b .
 
   .thumb_func
-  .weak AC_IRQHandler
-AC_IRQHandler:
+  .weak AC_Handler
+AC_Handler:
   b .
 
   .thumb_func
-  .weak DAC_IRQHandler
-DAC_IRQHandler:
+  .weak DAC_Handler
+DAC_Handler:
   b .
 
   .thumb_func
-  .weak I2S_IRQHandler
-I2S_IRQHandler:
+  .weak I2S_Handler
+I2S_Handler:
   b .
 
 #endif
@@ -291,34 +291,34 @@ _vectors:
   .word 0 /* Reserved */
   .word PendSV_Handler
   .word SysTick_Handler
-  .word PM_IRQHandler
-  .word SYSCTRL_IRQHandler
-  .word WDT_IRQHandler
-  .word RTC_IRQHandler
-  .word EIC_IRQHandler
-  .word NVMCTRL_IRQHandler
-  .word DMAC_IRQHandler
-  .word USB_IRQHandler
-  .word EVSYS_IRQHandler
-  .word SERCOM0_IRQHandler
-  .word SERCOM1_IRQHandler
-  .word SERCOM2_IRQHandler
-  .word SERCOM3_IRQHandler
-  .word SERCOM4_IRQHandler
-  .word SERCOM5_IRQHandler
-  .word TCC0_IRQHandler
-  .word TCC1_IRQHandler
-  .word TCC2_IRQHandler
-  .word TC3_IRQHandler
-  .word TC4_IRQHandler
-  .word TC5_IRQHandler
+  .word PM_Handler
+  .word SYSCTRL_Handler
+  .word WDT_Handler
+  .word RTC_Handler
+  .word EIC_Handler
+  .word NVMCTRL_Handler
+  .word DMAC_Handler
+  .word USB_Handler
+  .word EVSYS_Handler
+  .word SERCOM0_Handler
+  .word SERCOM1_Handler
+  .word SERCOM2_Handler
+  .word SERCOM3_Handler
+  .word SERCOM4_Handler
+  .word SERCOM5_Handler
+  .word TCC0_Handler
+  .word TCC1_Handler
+  .word TCC2_Handler
+  .word TC3_Handler
+  .word TC4_Handler
+  .word TC5_Handler
   .word Dummy_Handler /* Reserved */
   .word Dummy_Handler /* Reserved */
-  .word ADC_IRQHandler
-  .word AC_IRQHandler
-  .word DAC_IRQHandler
+  .word ADC_Handler
+  .word AC_Handler
+  .word DAC_Handler
   .word Dummy_Handler /* Reserved */
-  .word I2S_IRQHandler
+  .word I2S_Handler
 _vectors_end:
 
 #ifdef VECTORS_IN_RAM
