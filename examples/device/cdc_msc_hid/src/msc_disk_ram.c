d7e48f307bb77e8e6a55967d9b94c7218f267e83
hathach <thach@tinyusb.org>
2018-11-22 11:36:50 +0700

    clean up msc example

--- examples/device/cdc_msc_hid/src/msc_disk_ram.c
+++ examples/device/cdc_msc_hid/src/msc_disk_ram.c
@@ -36,9 +36,15 @@
  */
 /**************************************************************************/
 
-#include "msc_app.h"
+#include "bsp/board.h"
+#include "tusb.h"
 
 #if CFG_TUD_MSC
+
+#define README_CONTENTS \
+"This is tinyusb's MassStorage Class demo.\r\n\r\n\
+If you find any bugs or get any questions, feel free to file an\r\n\
+issue at github.com/hathach/tinyusb"
 
 enum
 {
