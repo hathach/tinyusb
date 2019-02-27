c51b11f103d7986bdeb277d8e7613b6a6b85364b
William D. Jones <thor0505@comcast.net>
2019-02-27 09:15:07 -0500

    Move all paths using "stm" to "st" subdirectories, update config.mk.

--- hw/bsp/stm32f407g_disc1/board.mk
+++ hw/bsp/stm32f407g_disc1/board.mk
@@ -14,14 +14,14 @@ LD_FILE = hw/bsp/stm32f407g_disc1/STM32F407VGTx_FLASH.ld
 LDFLAGS += -mthumb -mcpu=cortex-m4
 
 SRC_C += \
-	hw/mcu/stm/stm32f4/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
+	hw/mcu/st/stm32f4/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
 
 SRC_S += \
-	hw/mcu/stm/stm32f4/Device/ST/STM32F4xx/Source/Templates/gcc/startup_stm32f407xx.s
+	hw/mcu/st/stm32f4/Device/ST/STM32F4xx/Source/Templates/gcc/startup_stm32f407xx.s
 
 INC += \
-	-I$(TOP)/hw/mcu/stm/stm32f4/Device/ST/STM32F4xx/Include \
-	-I$(TOP)/hw/mcu/stm/stm32f4/Include
+	-I$(TOP)/hw/mcu/st/stm32f4/Device/ST/STM32F4xx/Include \
+	-I$(TOP)/hw/mcu/st/stm32f4/Include
 
-VENDOR = stm
+VENDOR = st
 CHIP_FAMILY = stm32f4
