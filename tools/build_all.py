30cc042f6e1bac43618bf2606e034ee1139162ff
hathach <thach@tinyusb.org>
2019-04-26 15:55:00 +0700

    include msc_dual_lun example to travis

--- tools/build_all.py
+++ tools/build_all.py
@@ -4,8 +4,10 @@ import sys
 import subprocess
 import time
 
+all_device_example = ["cdc_msc_hid", "msc_dual_lun"]
 all_boards = ["metro_m0_express", "metro_m4_express", "pca10056", "stm32f407g_disc1"]
 
-for board in all_boards:
-    subprocess.run("make -j2 -C examples/device/cdc_msc_hid BOARD={} clean".format(board), shell=True)
-    subprocess.run("make -j2 -C examples/device/cdc_msc_hid BOARD={} all".format(board), shell=True)
+for example in all_device_example:
+    for board in all_boards:
+        subprocess.run("make -j2 -C examples/device/{} BOARD={} clean".format(example, board), shell=True)
+        subprocess.run("make -j2 -C examples/device/{} BOARD={} all".format(example, board), shell=True)
