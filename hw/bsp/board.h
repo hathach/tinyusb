a619511631ea3ece5d419c5503b26d973c7016fa
hathach <thach@tinyusb.org>
2019-05-29 16:55:54 +0700

    run tud_task() in board_delay()

--- hw/bsp/board.h
+++ hw/bsp/board.h
@@ -98,7 +98,11 @@ static inline void board_led_off(void)
 static inline void board_delay(uint32_t ms)
 {
   uint32_t start_ms = board_millis();
-  while( board_millis() - start_ms < ms) {}
+  while (board_millis() - start_ms < ms)
+  {
+    // take chance to run usb background
+    tud_task();
+  }
 }
 
 static inline int8_t board_uart_getchar(void)
