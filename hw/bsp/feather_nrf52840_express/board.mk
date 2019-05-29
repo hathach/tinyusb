90fea785c6aa60fdfd398fee262bb06c69a010b7
hathach <thach@tinyusb.org>
2019-05-29 16:56:26 +0700

    use adafruit-nrfutil dfu as default flash target for feather nrf52840

--- hw/bsp/feather_nrf52840_express/board.mk
+++ hw/bsp/feather_nrf52840_express/board.mk
@@ -50,5 +50,10 @@ JLINK_IF = swd
 # For uf2 conversion
 UF2_FAMILY = 0xADA52840
 
-# flash using jlink
-flash: flash-jlink
+$(BUILD)/$(BOARD)-firmware.zip: $(BUILD)/$(BOARD)-firmware.hex
+	adafruit-nrfutil dfu genpkg --dev-type 0x0052 --sd-req 0xFFFE --application $^ $@
+	
+# flash using adafruit-nrfutil dfu
+flash: $(BUILD)/$(BOARD)-firmware.zip
+	@:$(call check_defined, SERIAL, example: SERIAL=/dev/ttyACM0)
+	adafruit-nrfutil --verbose dfu serial --package $^ -p $(SERIAL) -b 115200 --singlebank --touch 1200
