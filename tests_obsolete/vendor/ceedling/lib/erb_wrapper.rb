ede3f36f31a8b583dc912ce4f362cba44b95df5f
hathach <thach@tinyusb.org>
2019-06-10 17:42:27 +0700

    remove test_obsolete

--- tests_obsolete/vendor/ceedling/lib/erb_wrapper.rb
+++
@@ -1,9 +0,0 @@
-require 'erb'
-
-class ErbWrapper
-  def generate_file(template, data, output_file)
-    File.open(output_file, "w") do |f|
-      f << ERB.new(template, 0, "<>").result(binding)
-    end
-  end
-end
