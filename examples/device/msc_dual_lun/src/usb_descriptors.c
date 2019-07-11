90395f84cb81d8a4341d6a072dfb624cfa307f96
hathach <thach@tinyusb.org>
2019-07-11 22:36:00 +0700

    more 2.1

--- examples/device/msc_dual_lun/src/usb_descriptors.c
+++ examples/device/msc_dual_lun/src/usb_descriptors.c
@@ -39,7 +39,7 @@ tusb_desc_device_t const desc_device =
 {
     .bLength            = sizeof(tusb_desc_device_t),
     .bDescriptorType    = TUSB_DESC_DEVICE,
-    .bcdUSB             = 0x0200,
+    .bcdUSB             = 0x0210,
     .bDeviceClass       = 0x00,
     .bDeviceSubClass    = 0x00,
     .bDeviceProtocol    = 0x00,
