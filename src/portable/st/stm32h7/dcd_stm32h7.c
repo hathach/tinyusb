921016760354599ada9f4208d96b6413b0562419
William D. Jones <thor0505@comcast.net>
2019-09-02 21:00:30 -0400

    stm32h7: Implement dcd_set_address.

--- src/portable/st/stm32h7/dcd_stm32h7.c
+++ src/portable/st/stm32h7/dcd_stm32h7.c
@@ -190,7 +190,13 @@ void dcd_int_disable (uint8_t rhport)
 void dcd_set_address (uint8_t rhport, uint8_t dev_addr)
 {
   (void) rhport;
-  (void) dev_addr;
+
+  USB_OTG_DeviceTypeDef * dev = DEVICE_BASE;
+
+  dev->DCFG |= (dev_addr << USB_OTG_DCFG_DAD_Pos) & USB_OTG_DCFG_DAD_Msk;
+
+  // Response with status after changing device address
+  dcd_edpt_xfer(rhport, tu_edpt_addr(0, TUSB_DIR_IN), NULL, 0);
 }
 
 void dcd_set_config (uint8_t rhport, uint8_t config_num)
