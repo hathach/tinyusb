3497dd02642c92ad6116f0f59d7890e341d82a08
hathach <thach@tinyusb.org>
2019-09-09 16:47:57 +0700

    adding ngx4330

---
+++ hw/bsp/ngx4330/board.mk
@@ -0,0 +1,47 @@
+CFLAGS += \
+  -mthumb \
+  -mabi=aapcs \
+  -mcpu=cortex-m4 \
+  -nostdlib \
+  -DCORE_M4 \
+  -DCFG_TUSB_MCU=OPT_MCU_LPC43XX \
+  -D__USE_LPCOPEN
+
+# lpc_types.h cause following errors
+CFLAGS += -Wno-error=strict-prototypes
+
+MCU_DIR = hw/mcu/nxp/lpc_driver/lpc43xx/lpc_chip_43xx
+
+# All source paths should be relative to the top level.
+LD_FILE = hw/bsp/$(BOARD)/ngx4330.ld
+
+# TODO remove later
+SRC_C += src/portable/$(VENDOR)/$(CHIP_FAMILY)/hal_$(CHIP_FAMILY).c
+
+SRC_C += \
+	$(MCU_DIR)/../gcc/cr_startup_lpc43xx.c \
+	$(MCU_DIR)/src/chip_18xx_43xx.c \
+	$(MCU_DIR)/src/clock_18xx_43xx.c \
+	$(MCU_DIR)/src/gpio_18xx_43xx.c \
+	$(MCU_DIR)/src/sysinit_18xx_43xx.c \
+	$(MCU_DIR)/src/i2c_18xx_43xx.c \
+	$(MCU_DIR)/src/i2cm_18xx_43xx.c \
+	$(MCU_DIR)/src/uart_18xx_43xx.c
+
+INC += \
+	$(TOP)/$(MCU_DIR)/inc \
+	$(TOP)/$(MCU_DIR)/inc/config_43xx
+
+# For TinyUSB port source
+VENDOR = nxp
+CHIP_FAMILY = lpc18_43
+
+# For freeRTOS port source
+FREERTOS_PORT = ARM_CM4
+
+# For flash-jlink target
+JLINK_DEVICE = LPC4330
+JLINK_IF = swd 
+
+# flash using jlink
+flash: flash-jlink
