340dcb81bfc7074ca01bbee6177ea52562aa3b51
Nathan Conrad <pigrew@gmail.com>
2019-09-12 15:39:51 -0400

    For control transfers, compare the transmitted length against the requested length to know if a ZLP needs to happen. (fixes #139)

--- src/device/usbd_control.c
+++ src/device/usbd_control.c
@@ -43,8 +43,9 @@ typedef struct
   tusb_control_request_t request;
 
   void* buffer;
-  uint16_t total_len;
+  uint16_t len;
   uint16_t total_transferred;
+  uint16_t requested_len;
 
   bool (*complete_cb) (uint8_t, tusb_control_request_t const *);
 } usbd_control_xfer_t;
@@ -68,7 +69,7 @@ bool tud_control_status(uint8_t rhport, tusb_control_request_t const * request)
 // Each transaction is up to endpoint0's max packet size
 static bool start_control_data_xact(uint8_t rhport)
 {
-  uint16_t const xact_len = tu_min16(_control_state.total_len - _control_state.total_transferred, CFG_TUD_ENDPOINT0_SIZE);
+  uint16_t const xact_len = tu_min16(_control_state.len - _control_state.total_transferred, CFG_TUD_ENDPOINT0_SIZE);
 
   uint8_t ep_addr = EDPT_CTRL_OUT;
 
@@ -89,10 +90,16 @@ void usbd_control_set_complete_callback( bool (*fp) (uint8_t, tusb_control_reque
 
 bool tud_control_xfer(uint8_t rhport, tusb_control_request_t const * request, void* buffer, uint16_t len)
 {
+  // transmitted length must be <= requested length (USB 2.0 spec: 8.5.3.1 )
+  // FIXME: Should logic be here or in place that calls this function?
+  if(len > request->wLength)
+    len = request->wLength;
+
   _control_state.request = (*request);
   _control_state.buffer = buffer;
-  _control_state.total_len = tu_min16(len, request->wLength);
   _control_state.total_transferred = 0;
+  _control_state.requested_len = request->wLength;
+  _control_state.len = len;
 
   if ( len )
   {
@@ -124,7 +131,7 @@ bool usbd_control_xfer_cb (uint8_t rhport, uint8_t ep_addr, xfer_result_t result
   _control_state.total_transferred += xferred_bytes;
   _control_state.buffer += xferred_bytes;
 
-  if ( _control_state.total_len == _control_state.total_transferred || xferred_bytes < CFG_TUD_ENDOINT0_SIZE )
+  if ( (_control_state.requested_len == _control_state.total_transferred) || xferred_bytes < CFG_TUD_ENDOINT0_SIZE )
   {
     // DATA stage is complete
     bool is_ok = true;
