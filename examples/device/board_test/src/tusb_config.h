4ccdc5522d202daec98e2bb6536a4ed0ccd98b9a
Nathan Conrad <pigrew@gmail.com>
2019-09-27 12:41:46 -0400

    Fix typo of CFG_TUD_ENDOINT0_SIZE.

--- examples/device/board_test/src/tusb_config.h
+++ examples/device/board_test/src/tusb_config.h
@@ -64,7 +64,7 @@
 // DEVICE CONFIGURATION
 //--------------------------------------------------------------------
 
-#define CFG_TUD_ENDOINT0_SIZE   64
+#define CFG_TUD_ENDPOINT0_SIZE   64
 
 //------------- CLASS -------------//
 #define CFG_TUD_CDC             0
