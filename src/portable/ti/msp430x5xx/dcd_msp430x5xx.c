ea0e799df6594fa6644575493547f41994c9be27
William D. Jones <thor0505@comcast.net>
2019-09-29 02:17:59 -0400

    dcd_msp430x5xx: Fix clear stall logic.

--- src/portable/ti/msp430x5xx/dcd_msp430x5xx.c
+++ src/portable/ti/msp430x5xx/dcd_msp430x5xx.c
@@ -337,11 +337,11 @@ void dcd_edpt_clear_stall (uint8_t rhport, uint8_t ep_addr)
   {
     if(dir == TUSB_DIR_OUT)
     {
-      USBOEPCNT_0 &= ~NAK;
+      USBOEPCNT_0 &= ~STALL;
     }
     else
     {
-      USBIEPCNT_0 &= ~NAK;
+      USBIEPCNT_0 &= ~STALL;
     }
   }
 }
