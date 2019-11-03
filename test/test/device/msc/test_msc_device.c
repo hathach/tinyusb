66ce39368e0f4bba366956d3b0cd7a9ef87168b8
hathach <thach@tinyusb.org>
2019-11-04 00:10:46 +0700

    clean up

--- test/test/device/msc/test_msc_device.c
+++ test/test/device/msc/test_msc_device.c
@@ -1,7 +1,7 @@
 /* 
  * The MIT License (MIT)
  *
- * Copyright (c) 2019 hathach for Adafruit Industries
+ * Copyright (c) 2019, hathach (tinyusb.org)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +20,10 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
+ *
+ * This file is part of the TinyUSB stack.
  */
+
 
 #include "unity.h"
 
