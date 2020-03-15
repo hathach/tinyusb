aafddfe637cf2dadf82075b59b18aad6a722caaa
Peter Lawrence <12226419+majbthrd@users.noreply.github.com>
2020-03-15 18:32:02 -0500

    following suggestion by @kasjer

--- src/class/cdc/cdc_device.c
+++ src/class/cdc/cdc_device.c
@@ -413,7 +413,7 @@ bool cdcd_xfer_cb(uint8_t rhport, uint8_t ep_addr, xfer_result_t result, uint32_
   // Though maybe the baudrate is not really important !!!
   if ( ep_addr == p_cdc->ep_in )
   {
-    if ( xferred_bytes && (0 == (xferred_bytes % CFG_TUD_CDC_EPSIZE)) ) usbd_edpt_xfer(TUD_OPT_RHPORT, p_cdc->ep_in, NULL, 0);
+    if ( xferred_bytes && (0 == (xferred_bytes % CFG_TUD_CDC_EPSIZE)) && (0 == p_cdc->tx_ff.count) ) usbd_edpt_xfer(TUD_OPT_RHPORT, p_cdc->ep_in, NULL, 0);
   }
 
   // nothing to do with notif endpoint for now
