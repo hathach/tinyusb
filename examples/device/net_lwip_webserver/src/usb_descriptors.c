aa01e5f447376f407a114c153ecae488a9498b98
hathach <thach@tinyusb.org>
2020-03-23 23:46:12 +0700

    enhance string index

--- examples/device/net_lwip_webserver/src/usb_descriptors.c
+++ examples/device/net_lwip_webserver/src/usb_descriptors.c
@@ -36,6 +36,17 @@
 #define USB_PID           (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | \
                            _PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4) | _PID_MAP(NET, 5) )
 
+// String Descriptor Index
+enum
+{
+  STRID_LANGID = 0,
+  STRID_MANUFACTURER,
+  STRID_PRODUCT,
+  STRID_SERIAL,
+  STRID_INTERFACE,
+  STRID_MAC
+};
+
 //--------------------------------------------------------------------+
 // Device Descriptors
 //--------------------------------------------------------------------+
@@ -54,9 +65,9 @@ tusb_desc_device_t const desc_device =
     .idProduct          = USB_PID,
     .bcdDevice          = 0x0100,
 
-    .iManufacturer      = 0x01,
-    .iProduct           = 0x02,
-    .iSerialNumber      = 0x03,
+    .iManufacturer      = STRID_MANUFACTURER,
+    .iProduct           = STRID_PRODUCT,
+    .iSerialNumber      = STRID_SERIAL,
 
     .bNumConfigurations = 0x01
 };
@@ -78,21 +89,12 @@ enum
   ITF_NUM_TOTAL
 };
 
-enum
-{
-  STR_LANGID = 0,
-  STR_MANUFACTURER,
-  STR_PRODUCT,
-  STR_ITFNAME,
-  STR_MAC,
-};
-
 #if CFG_TUD_NET == OPT_NET_ECM
-#define CONFIG_TOTAL_LEN    (TUD_CONFIG_DESC_LEN + TUD_CDC_ECM_DESC_LEN)
+  #define CONFIG_TOTAL_LEN    (TUD_CONFIG_DESC_LEN + TUD_CDC_ECM_DESC_LEN)
 #elif CFG_TUD_NET == OPT_NET_RNDIS
-#define CONFIG_TOTAL_LEN    (TUD_CONFIG_DESC_LEN + TUD_RNDIS_DESC_LEN)
+  #define CONFIG_TOTAL_LEN    (TUD_CONFIG_DESC_LEN + TUD_RNDIS_DESC_LEN)
 #elif CFG_TUD_NET == OPT_NET_EEM
-#define CONFIG_TOTAL_LEN    (TUD_CONFIG_DESC_LEN + TUD_CDC_EEM_DESC_LEN)
+  #define CONFIG_TOTAL_LEN    (TUD_CONFIG_DESC_LEN + TUD_CDC_EEM_DESC_LEN)
 #endif
 
 #if CFG_TUSB_MCU == OPT_MCU_LPC175X_6X || CFG_TUSB_MCU == OPT_MCU_LPC177X_8X || CFG_TUSB_MCU == OPT_MCU_LPC40XX
@@ -110,13 +112,13 @@ uint8_t const desc_configuration[] =
 
 #if CFG_TUD_NET == OPT_NET_ECM
   // Interface number, description string index, MAC address string index, EP notification address and size, EP data address (out, in), and size, max segment size.
-  TUD_CDC_ECM_DESCRIPTOR(ITF_NUM_CDC, STR_ITFNAME, STR_MAC, 0x81, 8, EPNUM_CDC, 0x80 | EPNUM_CDC, CFG_TUD_NET_ENDPOINT_SIZE, CFG_TUD_NET_MTU),
+  TUD_CDC_ECM_DESCRIPTOR(ITF_NUM_CDC, STRID_INTERFACE, STRID_MAC, 0x81, 8, EPNUM_CDC, 0x80 | EPNUM_CDC, CFG_TUD_NET_ENDPOINT_SIZE, CFG_TUD_NET_MTU),
 #elif CFG_TUD_NET == OPT_NET_RNDIS
   // Interface number, string index, EP notification address and size, EP data address (out, in) and size.
-  TUD_RNDIS_DESCRIPTOR(ITF_NUM_CDC, STR_ITFNAME, 0x81, 8, EPNUM_CDC, 0x80 | EPNUM_CDC, CFG_TUD_NET_ENDPOINT_SIZE),
+  TUD_RNDIS_DESCRIPTOR(ITF_NUM_CDC, STRID_INTERFACE, 0x81, 8, EPNUM_CDC, 0x80 | EPNUM_CDC, CFG_TUD_NET_ENDPOINT_SIZE),
 #elif CFG_TUD_NET == OPT_NET_EEM
   // Interface number, description string index, EP data address (out, in) and size.
-  TUD_CDC_EEM_DESCRIPTOR(ITF_NUM_CDC, STR_ITFNAME, EPNUM_CDC, 0x80 | EPNUM_CDC, CFG_TUD_NET_ENDPOINT_SIZE),
+  TUD_CDC_EEM_DESCRIPTOR(ITF_NUM_CDC, STRID_INTERFACE, EPNUM_CDC, 0x80 | EPNUM_CDC, CFG_TUD_NET_ENDPOINT_SIZE),
 #endif
 };
 
@@ -136,17 +138,13 @@ uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
 // array of pointer to string descriptors
 char const* string_desc_arr [] =
 {
-  [STR_LANGID]       = (const char[]) { 0x09, 0x04 }, // supported language is English (0x0409)
-  [STR_MANUFACTURER] = "TinyUSB",                     // Manufacturer
-  [STR_PRODUCT]      = "TinyUSB Device",              // Product
-  [STR_ITFNAME]      =                                // CDC-ECM Interface
-#if CFG_TUD_NET == OPT_NET_ECM
-                       "TinyUSB CDC-ECM",
-#elif CFG_TUD_NET == OPT_NET_RNDIS
-                       "TinyUSB RNDIS",
-#elif CFG_TUD_NET == OPT_NET_EEM
-                       "TinyUSB CDC-EEM",
-#endif
+  [STRID_LANGID]       = (const char[]) { 0x09, 0x04 }, // supported language is English (0x0409)
+  [STRID_MANUFACTURER] = "TinyUSB",                     // Manufacturer
+  [STRID_PRODUCT]      = "TinyUSB Device",              // Product
+  [STRID_SERIAL]       = "123456",                      // Serials
+  [STRID_INTERFACE]    = "TinyUSB Network Interface"    // CDC-ECM Interface
+
+  // STRID_MAC index is handled separately
 };
 
 static uint16_t _desc_str[32];
@@ -159,13 +157,12 @@ uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid)
 
   unsigned int chr_count = 0;
 
-  if (STR_LANGID == index)
+  if (STRID_LANGID == index)
   {
     memcpy(&_desc_str[1], string_desc_arr[0], 2);
     chr_count = 1;
   }
-#if CFG_TUD_NET == OPT_NET_ECM
-  else if (STR_MAC == index)
+  else if (STRID_MAC == index)
   {
     // Convert MAC address into UTF-16
 
@@ -175,7 +172,6 @@ uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid)
       _desc_str[1+chr_count++] = "0123456789ABCDEF"[(tud_network_mac_address[i] >> 0) & 0xf];
     }
   }
-#endif
   else
   {
     // Convert ASCII string into UTF-16
