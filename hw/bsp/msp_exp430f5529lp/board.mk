7e78e47444c29b4d0c804877b28aa778fa0576c3
William D. Jones <thor0505@comcast.net>
2020-03-24 00:40:25 -0400

    msp_exp430f5529lp: Fix board.mk, remove .travis.yml.bck.

--- hw/bsp/msp_exp430f5529lp/board.mk
+++ hw/bsp/msp_exp430f5529lp/board.mk
@@ -9,6 +9,7 @@ CFLAGS += \
 # All source paths should be relative to the top level.
 LD_FILE = hw/mcu/ti/msp430/msp430-gcc-support-files/include/msp430f5529.ld
 LDINC += $(TOP)/hw/mcu/ti/msp430/msp430-gcc-support-files/include
+LDFLAGS += $(addprefix -L,$(LDINC))
 
 INC += $(TOP)/hw/mcu/ti/msp430/msp430-gcc-support-files/include
 
@@ -16,17 +17,17 @@ INC += $(TOP)/hw/mcu/ti/msp430/msp430-gcc-support-files/include
 VENDOR = ti
 CHIP_FAMILY = msp430x5xx
 
-# Allow user to use mspdebug if set on make command line.
-ifdef MSPDEBUG
-# flash target using mspdebug.
-flash: $(BUILD)/$(BOARD)-firmware.elf
-	$(MSPDEBUG) tilib "prog $<" --allow-fw-update
-else
 # export for libmsp430.so to same installation
+ifneq ($(OS),Windows_NT)
 export LD_LIBRARY_PATH=$(dir $(shell which MSP430Flasher))
+endif
+
 # flash target using TI MSP430-Flasher
 # http://www.ti.com/tool/MSP430-FLASHER
 # Please add its installation dir to PATH
 flash: $(BUILD)/$(BOARD)-firmware.hex
 	MSP430Flasher -w $< -z [VCC]
-endif
+
+# flash target using mspdebug.
+flash-mspdebug: $(BUILD)/$(BOARD)-firmware.elf
+	$(MSPDEBUG) tilib "prog $<" --allow-fw-update
