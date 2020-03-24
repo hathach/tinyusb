27efedc9e6304d429dbf9b4f481d1241cf310210
William D. Jones <thor0505@comcast.net>
2020-03-23 23:45:22 -0400

    msp_exp430f5529lp: Allow user to choose mspdebug as alternate programmer.

--- hw/bsp/msp_exp430f5529lp/board.mk
+++ hw/bsp/msp_exp430f5529lp/board.mk
@@ -16,11 +16,17 @@ INC += $(TOP)/hw/mcu/ti/msp430/msp430-gcc-support-files/include
 VENDOR = ti
 CHIP_FAMILY = msp430x5xx
 
+# Allow user to use mspdebug if set on make command line.
+ifdef MSPDEBUG
+# flash target using mspdebug.
+flash: $(BUILD)/$(BOARD)-firmware.elf
+	$(MSPDEBUG) tilib "prog $<" --allow-fw-update
+else
 # export for libmsp430.so to same installation
 export LD_LIBRARY_PATH=$(dir $(shell which MSP430Flasher))
-
 # flash target using TI MSP430-Flasher
 # http://www.ti.com/tool/MSP430-FLASHER
 # Please add its installation dir to PATH
 flash: $(BUILD)/$(BOARD)-firmware.hex
 	MSP430Flasher -w $< -z [VCC]
+endif
