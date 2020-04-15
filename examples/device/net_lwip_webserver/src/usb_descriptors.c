dc9f5cc91a19f0902e78d0d662f1ba920dd4ab4e
hathach <thach@tinyusb.org>
2020-04-15 18:09:30 +0700

    remove unused usb_descriptors.h

--- examples/device/net_lwip_webserver/src/usb_descriptors.c
+++ examples/device/net_lwip_webserver/src/usb_descriptors.c
@@ -24,7 +24,6 @@
  */
 
 #include "tusb.h"
-#include "usb_descriptors.h"
 
 /* A combination of interfaces must have a unique product id, since PC will save device driver after the first plug.
  * Same VID/PID with different interface e.g MSC (first), then CDC (later) will possibly cause system error on PC.
