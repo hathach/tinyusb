99df7789a75f739cf5dd2338dbd2597bab7ec2ab
Jan Dümpelmann <jan.duempelmann@inotec-licht.de>
2020-06-29 10:55:03 +0200

    Add author name to dcd_synopsys.c

--- src/portable/st/synopsys/dcd_synopsys.c
+++ src/portable/st/synopsys/dcd_synopsys.c
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2018 Scott Shawcroft, 2019 William D. Jones for Adafruit Industries
  * Copyright (c) 2019 Ha Thach (tinyusb.org)
+ * Copyright (c) 2020 Jan Duempelmann
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
