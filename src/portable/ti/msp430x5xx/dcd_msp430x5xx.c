6976e642176cf76be9cc1bd2ef1a137a0e71e078
hathach <thach@tinyusb.org>
2020-07-28 14:36:15 +0700

    fix msp430 gcc 9.2.0 warning in #465

--- src/portable/ti/msp430x5xx/dcd_msp430x5xx.c
+++ src/portable/ti/msp430x5xx/dcd_msp430x5xx.c
@@ -70,8 +70,7 @@ typedef enum
   SIZXY = 7
 } ep_regs_index_t;
 
-#define EP_REGS(epnum, dir) &USBOEPCNF_1 + 64*dir + 8*(epnum - 1)
-
+#define EP_REGS(epnum, dir) ((ep_regs_t) ((uintptr_t)&USBOEPCNF_1 + 64*dir + 8*(epnum - 1)))
 
 static void bus_reset(void)
 {
