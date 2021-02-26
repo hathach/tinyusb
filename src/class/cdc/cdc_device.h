55a46a5c3b7ad3afb70a995954baa6148381b3a5
boggyb <amitv87@gmail.com>
2021-02-26 11:00:34 +0530

    Update cdc_device.h
    Minor api callback change as requested

--- src/class/cdc/cdc_device.h
+++ src/class/cdc/cdc_device.h
@@ -133,9 +133,6 @@ static inline bool     tud_cdc_write_clear     (void);
 // Invoked when received new data
 TU_ATTR_WEAK void tud_cdc_rx_cb(uint8_t itf);
 
-// Invoked when received send break
-TU_ATTR_WEAK void tud_cdc_send_break_cb(uint8_t itf, uint16_t wait_ms);
-
 // Invoked when received `wanted_char`
 TU_ATTR_WEAK void tud_cdc_rx_wanted_cb(uint8_t itf, char wanted_char);
 
@@ -147,6 +144,9 @@ TU_ATTR_WEAK void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts);
 
 // Invoked when line coding is change via SET_LINE_CODING
 TU_ATTR_WEAK void tud_cdc_line_coding_cb(uint8_t itf, cdc_line_coding_t const* p_line_coding);
+
+// Invoked when received send break
+TU_ATTR_WEAK void tud_cdc_send_break_cb(uint8_t itf, uint16_t duration_ms);
 
 //--------------------------------------------------------------------+
 // Inline Functions
