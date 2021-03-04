143582870c8a4175706109e238f4cfa814ae90bc
Rafael Silva <silvagracarafael@gmail.com>
2021-03-04 23:00:48 +0000

    add efm32gg12 family
    Signed-off-by: Rafael Silva <silvagracarafael@gmail.com>

--- hw/bsp/board_mcu.h
+++ hw/bsp/board_mcu.h
@@ -119,6 +119,9 @@
 
 #elif CFG_TUSB_MCU == OPT_MCU_RP2040
   #include "pico.h"
+  
+#elif CFG_TUSB_MCU == OPT_MCU_EFM32GG || CFG_TUSB_MCU == OPT_MCU_EFM32GG11 || CFG_TUSB_MCU == OPT_MCU_EFM32GG12
+  #include "em_device.h"
 
 #else
   #error "Missing MCU header"
