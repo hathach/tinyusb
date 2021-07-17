f52e1889c7f4bca23311a97b5869eab70c766094
MasterPhi <admin@hifiphile.com>
2021-07-17 20:41:19 +0200

    Fix merge error.

--- examples/device/hid_composite/src/tusb_config.h
+++ examples/device/hid_composite/src/tusb_config.h
@@ -69,7 +69,7 @@
 #endif
 
 // CFG_TUSB_DEBUG is defined by compiler in DEBUG build
-#define CFG_TUSB_DEBUG           0
+// #define CFG_TUSB_DEBUG           0
 
 /* USB DMA on some MCUs can only access a specific SRAM region with restriction on alignment.
  * Tinyusb use follows macros to declare transferring memory so that they can be put
