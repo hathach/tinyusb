c050612142ba1ab52cce7c7141230d5108e9936b
hathach <thach@tinyusb.org>
2021-08-17 13:33:15 +0700

    fix ci build

--- examples/device/hid_boot_interface/src/main.c
+++ examples/device/hid_boot_interface/src/main.c
@@ -207,6 +207,8 @@ uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_t
 // received data on OUT endpoint ( Report ID = 0, Type = 0 )
 void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize)
 {
+  (void) report_id;
+
   // keyboard interface
   if (instance == ITF_NUM_KEYBOARD)
   {
