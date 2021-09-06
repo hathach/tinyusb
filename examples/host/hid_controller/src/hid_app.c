c53ce53058fc4d2cd7928836cdc2a83615287f3e
Christian Arlt <arlt_c@arcor.de>
2021-09-06 19:38:31 +0200

    Added Sony DualShock4 CUH-ZCT1E pid

--- examples/host/hid_controller/src/hid_app.c
+++ examples/host/hid_controller/src/hid_app.c
@@ -111,7 +111,7 @@ static inline bool is_sony_ds4(uint8_t dev_addr)
   uint16_t vid, pid;
   tuh_vid_pid_get(dev_addr, &vid, &pid);
 
-  return (vid == 0x054c && pid == 0x09cc);
+  return ( (vid == 0x054c && (pid == 0x09cc || pid == 0x05c4)) ); // Sony DualShock4 
 }
 
 //--------------------------------------------------------------------+
