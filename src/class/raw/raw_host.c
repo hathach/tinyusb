461deaffb5fefe20a81098f3e0c64c2e235585e0
BlueChip <csbluechip@gmail.com>
2021-09-08 01:42:28 +0100

    bug workaround - last entry in descriptor list sometimes points at itself ; tag TODO ; output cleanup

--- src/class/raw/raw_host.c
+++ src/class/raw/raw_host.c
@@ -66,118 +66,130 @@ void  raw_init (void)
 //
 bool  raw_open (uint8_t rhport,  uint8_t dev_addr,  tusb_desc_interface_t const *itf_desc,  uint16_t max_len)
 {
-	uint16_t  vid, pid;
-
-//	raw_open_pre_cb(rhport, dev_addr, itf_desc, max_len);
-
 	uint8_t const*  pDesc = NULL;
+	uint8_t const*  pLast = NULL;  // BUG: Sometimes the last descriptor points at itself as being 'next'
+
+	uint16_t        vid   = 0x0000;
+	uint16_t        pid   = 0x0000;
 
 	printf("# raw_open(port=%d, dev=%d, *desc=%p, max=%d)\r\n", rhport, dev_addr, itf_desc, max_len);
-	// src/common/tusb_types.h
-	for (pDesc = (void*)itf_desc;  tu_desc_type(pDesc);  pDesc = tu_desc_next(pDesc)) {
-		printf("    (pDesc)    = %p\r\n", pDesc);
-		switch (tu_desc_type(pDesc)) {
-			case TUSB_DESC_DEVICE                           : {  // 0x01,
-				printf("TUSB_DESC_DEVICE\r\n");
-				break;
-			}
-			case TUSB_DESC_CONFIGURATION                    : {  // 0x02,
-				printf("TUSB_DESC_CONFIGURATION\r\n");
-				break;
-			}
-			case TUSB_DESC_STRING                           : {  // 0x03,
-				printf("TUSB_DESC_STRING\r\n");
-				break;
-			}
-			case TUSB_DESC_INTERFACE                        : {  // 0x04,
-				tusb_desc_interface_t const* p = (tusb_desc_interface_t const*)pDesc;
-				printf("    desc.len   = %d\r\n" , p->bLength           );  // descriptor size
-				printf("    desc.type  = %02X - ", p->bDescriptorType   );  // Descriptor Type
-				printf("TUSB_DESC_INTERFACE\r\n");
-				printf("    desc.ifNum = %d\r\n" , p->bInterfaceNumber  );  // interface number (zero based)
-				printf("    desc.alt   = %d\r\n" , p->bAlternateSetting );  // value to select this setting
-				printf("    desc.epCnt = %d\r\n" , p->bNumEndpoints     );  // number of endpoints (not couting ep0)
-				printf("    desc.Class = %d\r\n" , p->bInterfaceClass   );  // USB-IF class code
-				printf("    desc.SubCl = %d\r\n" , p->bInterfaceSubClass);  // USB-IF subclass code
-				printf("    desc.proto = %d\r\n" , p->bInterfaceProtocol);  // protocol code
-				printf("    desc.ifStr = %d\r\n" , p->iInterface        );  // index of string
-				break;
-			}
-			case TUSB_DESC_ENDPOINT                         : {  // 0x05,
-				printf("TUSB_DESC_ENDPOINT\r\n");
-				break;
-			}
-			case TUSB_DESC_DEVICE_QUALIFIER                 : {  // 0x06,
-				printf("TUSB_DESC_DEVICE_QUALIFIER\r\n");
-				break;
-			}
-			case TUSB_DESC_OTHER_SPEED_CONFIG               : {  // 0x07,
-				printf("TUSB_DESC_OTHER_SPEED_CONFIG\r\n");
-				break;
-			}
-			case TUSB_DESC_INTERFACE_POWER                  : {  // 0x08,
-				printf("TUSB_DESC_INTERFACE_POWER\r\n");
-				break;
-			}
-			case TUSB_DESC_OTG                              : {  // 0x09,
-				printf("TUSB_DESC_OTG\r\n");
-				break;
-			}
-			case TUSB_DESC_DEBUG                            : {  // 0x0A,
-				printf("TUSB_DESC_DEBUG\r\n");
-				break;
-			}
-			case TUSB_DESC_INTERFACE_ASSOCIATION            : {  // 0x0B,
-				printf("TUSB_DESC_INTERFACE_ASSOCIATION\r\n");
-				break;
-			}
-			case TUSB_DESC_BOS                              : {  // 0x0F,
-				printf("TUSB_DESC_BOS\r\n");
-				break;
-			}
-			case TUSB_DESC_DEVICE_CAPABILITY                : {  // 0x10,
-				printf("TUSB_DESC_DEVICE_CAPABILITY\r\n");
-				break;
-			}
-			case TUSB_DESC_FUNCTIONAL                       : {  // 0x21,
-			//case TUSB_DESC_CS_DEVICE                        : {  // 0x21,
-				printf("TUSB_DESC_FUNCTIONAL / TUSB_DESC_CS_DEVICE\r\n");
-				break;
-			}
-			case TUSB_DESC_CS_CONFIGURATION                 : {  // 0x22,
-				printf("TUSB_DESC_CS_CONFIGURATION\r\n");
-				break;
-			}
-			case TUSB_DESC_CS_STRING                        : {  // 0x23,
-				printf("TUSB_DESC_CS_STRING\r\n");
-				break;
-			}
-			case TUSB_DESC_CS_INTERFACE                     : {  // 0x24,
-				printf("TUSB_DESC_CS_INTERFACE\r\n");
-				break;
-			}
-			case TUSB_DESC_CS_ENDPOINT                      : {  // 0x25,
-				printf("TUSB_DESC_CS_ENDPOINT\r\n");
-				break;
-			}
-			case TUSB_DESC_SUPERSPEED_ENDPOINT_COMPANION    : {  // 0x30,
-				printf("TUSB_DESC_SUPERSPEED_ENDPOINT_COMPANION\r\n");
-				break;
-			}
-			case TUSB_DESC_SUPERSPEED_ISO_ENDPOINT_COMPANION: {  // 0x31
-				printf("TUSB_DESC_SUPERSPEED_ISO_ENDPOINT_COMPANION\r\n");
-				break;
-			}
-			default:
-				printf("    Unknown Type: %02X\r\n", tu_desc_type(pDesc));
-				break;
-		}
-		printf("\r\n");
-	}
+//	raw_open_pre_cb(rhport, dev_addr, itf_desc, max_len);
 
 	// Why does this FAIL (only) the first time it is called? ...What is not initialised properly?
 	tuh_vid_pid_get(dev_addr, &vid, &pid);
 	printf("  VID=%04X, PID=%04X\r\n", vid, pid);
+
+	// src/common/tusb_types.h
+//	for (pDesc = (void*)itf_desc;  tu_desc_type(pDesc);  pDesc = tu_desc_next(pDesc)) {
+	for (pDesc = (void*)itf_desc;  tu_desc_type(pDesc) && (pDesc != pLast);  pDesc = tu_desc_next((pLast = pDesc))) {
+		//printf("  (pDesc) = 0x%p\r\n", pDesc);
+		switch (tu_desc_type(pDesc)) {
+			case TUSB_DESC_DEVICE                           : {  // 0x01,
+				printf("    TUSB_DESC_DEVICE [%02X]\r\n", tu_desc_type(pDesc));
+//!TODO 
+				break;
+			}
+			case TUSB_DESC_CONFIGURATION                    : {  // 0x02,
+				printf("    TUSB_DESC_CONFIGURATION [%02X]\r\n", tu_desc_type(pDesc));
+//!TODO 
+				break;
+			}
+			case TUSB_DESC_STRING                           : {  // 0x03,
+				printf("    TUSB_DESC_STRING [%02X]\r\n", tu_desc_type(pDesc));
+//!TODO 
+				break;
+			}
+			case TUSB_DESC_INTERFACE                        : {  // 0x04,
+				printf("    TUSB_DESC_INTERFACE [%02X]\r\n", tu_desc_type(pDesc));
+				tusb_desc_interface_t const* p = (tusb_desc_interface_t const*)pDesc;
+				printf("      desc.len   = %d\r\n"   , p->bLength           );  // descriptor size
+				printf("      desc.type  = x%02X\r\n", p->bDescriptorType   );  // Descriptor Type
+				printf("      desc.ifNum = %d\r\n"   , p->bInterfaceNumber  );  // interface number (zero based)
+				printf("      desc.alt   = %d\r\n"   , p->bAlternateSetting );  // value to select this setting
+				printf("      desc.epCnt = %d\r\n"   , p->bNumEndpoints     );  // number of endpoints (not couting ep0)
+				printf("      desc.Class = %d\r\n"   , p->bInterfaceClass   );  // USB-IF class code
+				printf("      desc.SubCl = %d\r\n"   , p->bInterfaceSubClass);  // USB-IF subclass code
+				printf("      desc.proto = %d\r\n"   , p->bInterfaceProtocol);  // protocol code
+				printf("      desc.ifStr = %d\r\n"   , p->iInterface        );  // index of string
+				break;
+			}
+			case TUSB_DESC_ENDPOINT                         : {  // 0x05,
+				printf("    TUSB_DESC_ENDPOINT [%02X]\r\n", tu_desc_type(pDesc));
+//!TODO - MIDI
+				break;
+			}
+			case TUSB_DESC_DEVICE_QUALIFIER                 : {  // 0x06,
+				printf("    TUSB_DESC_DEVICE_QUALIFIER [%02X]\r\n", tu_desc_type(pDesc));
+				break;
+			}
+			case TUSB_DESC_OTHER_SPEED_CONFIG               : {  // 0x07,
+				printf("    TUSB_DESC_OTHER_SPEED_CONFIG [%02X]\r\n", tu_desc_type(pDesc));
+				break;
+			}
+			case TUSB_DESC_INTERFACE_POWER                  : {  // 0x08,
+				printf("    TUSB_DESC_INTERFACE_POWER [%02X]\r\n", tu_desc_type(pDesc));
+				break;
+			}
+			case TUSB_DESC_OTG                              : {  // 0x09,
+				printf("    TUSB_DESC_OTG [%02X]\r\n", tu_desc_type(pDesc));
+				break;
+			}
+			case TUSB_DESC_DEBUG                            : {  // 0x0A,
+				printf("    TUSB_DESC_DEBUG [%02X]\r\n", tu_desc_type(pDesc));
+				break;
+			}
+			case TUSB_DESC_INTERFACE_ASSOCIATION            : {  // 0x0B,
+				printf("    TUSB_DESC_INTERFACE_ASSOCIATION [%02X]\r\n", tu_desc_type(pDesc));
+				break;
+			}
+			case TUSB_DESC_BOS                              : {  // 0x0F,
+				printf("    TUSB_DESC_BOS [%02X]\r\n", tu_desc_type(pDesc));
+				break;
+			}
+			case TUSB_DESC_DEVICE_CAPABILITY                : {  // 0x10,
+				printf("    TUSB_DESC_DEVICE_CAPABILITY [%02X]\r\n", tu_desc_type(pDesc));
+				break;
+			}
+			case TUSB_DESC_FUNCTIONAL                       : {  // 0x21,
+			//case TUSB_DESC_CS_DEVICE                        : {  // 0x21,
+				printf("    TUSB_DESC_FUNCTIONAL / TUSB_DESC_CS_DEVICE [%02X]\r\n", tu_desc_type(pDesc));
+//!TODO 
+				break;
+			}
+			case TUSB_DESC_CS_CONFIGURATION                 : {  // 0x22,
+				printf("    TUSB_DESC_CS_CONFIGURATION [%02X]\r\n", tu_desc_type(pDesc));
+//!TODO 
+				break;
+			}
+			case TUSB_DESC_CS_STRING                        : {  // 0x23,
+				printf("    TUSB_DESC_CS_STRING [%02X]\r\n", tu_desc_type(pDesc));
+//!TODO 
+				break;
+			}
+			case TUSB_DESC_CS_INTERFACE                     : {  // 0x24,
+				printf("    TUSB_DESC_CS_INTERFACE [%02X]\r\n", tu_desc_type(pDesc));
+//!TODO - MIDI
+				break;
+			}
+			case TUSB_DESC_CS_ENDPOINT                      : {  // 0x25,
+				printf("    TUSB_DESC_CS_ENDPOINT [%02X]\r\n", tu_desc_type(pDesc));
+//!TODO - MIDI
+				break;
+			}
+			case TUSB_DESC_SUPERSPEED_ENDPOINT_COMPANION    : {  // 0x30,
+				printf("    TUSB_DESC_SUPERSPEED_ENDPOINT_COMPANION [%02X]\r\n", tu_desc_type(pDesc));
+				break;
+			}
+			case TUSB_DESC_SUPERSPEED_ISO_ENDPOINT_COMPANION: {  // 0x31
+				printf("    TUSB_DESC_SUPERSPEED_ISO_ENDPOINT_COMPANION [%02X]\r\n", tu_desc_type(pDesc));
+				break;
+			}
+			default:
+				printf("    Unknown Type [%02X]\r\n", tu_desc_type(pDesc));
+				break;    
+		}
+	}
+
 
 
 
