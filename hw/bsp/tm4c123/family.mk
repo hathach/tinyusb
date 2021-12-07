8e0400d531bbbb8faf45576a3b5e55be9406cf6b
hathach <thach@tinyusb.org>
2021-12-07 16:28:24 +0700

    change uart baudrate for tm4c123 to 115200

--- hw/bsp/tm4c123/family.mk
+++ hw/bsp/tm4c123/family.mk
@@ -28,6 +28,7 @@ INC += \
 
 SRC_C += \
 	src/portable/mentor/musb/dcd_musb.c \
+	src/portable/mentor/musb/hcd_musb.c \
 	$(MCU_DIR)/Source/system_TM4C123.c \
 	$(MCU_DIR)/Source/GCC/tm4c123_startup.c
 
