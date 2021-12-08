bcbcdf87de303a4db1724de2a813b80f13d7d906
Gordon McNab <gordon.mcnab@brtchip.com>
2021-12-08 11:24:39 +0000

    Fix spelling mistakes and verify endpoint numbering.

--- examples/device/cdc_msc/src/usb_descriptors.c
+++ examples/device/cdc_msc/src/usb_descriptors.c
@@ -118,7 +118,7 @@ enum
   #define EPNUM_MSC_IN      0x84
 
 #elif CFG_TUSB_MCU == OPT_MCU_FT90X || CFG_TUSB_MCU == OPT_MCU_FT93X
-  // FT9XX don't support a same endpoint number with different direction IN and OUT
+  // FT9XX doesn't support a same endpoint number with different direction IN and OUT
   //    e.g EP1 OUT & EP1 IN cannot exist together
   #define EPNUM_CDC_NOTIF   0x81
   #define EPNUM_CDC_OUT     0x02
