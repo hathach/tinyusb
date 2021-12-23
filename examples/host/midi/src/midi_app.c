fe36bd56cd4570887ad0c561217e2f329a4398bc
rppicomidi <rppicomidi@gmail.com>
2021-12-22 16:52:19 -0800

    Make example more keyboard friendly

--- examples/host/midi/src/midi_app.c
+++ examples/host/midi/src/midi_app.c
@@ -23,32 +23,57 @@
  *
  */
 
+/**
+ * This test program is designed to send and receive MIDI data concurrently
+ * It send G, G#, A, A#, B over and over again on the highest cable number
+ * of the MIDI device. The notes correspond to the Mackie Control transport
+ * button LEDs, so most keyboards and controllers will react to these note
+ * messages from the host. The host will print on the serial port console
+ * any incoming messages along with the cable number that sent them.
+ *
+ * If you define MIDIH_TEST_KORG_NANOKONTROL2 to 1 and connect a Korg
+ * nanoKONTROL2 controller to the host, then the host will request a dump
+ * of the current scene, and the nanoKONTROL2 will respond with a long
+ * sysex message (used to prove that sysex decoding appears to work)
+ */
 #include "bsp/board.h"
 #include "tusb.h"
 #include "class/midi/midi_host.h"
-
 //--------------------------------------------------------------------+
 // MACRO TYPEDEF CONSTANT ENUM DECLARATION
 //--------------------------------------------------------------------+
+#ifndef MIDIH_TEST_KORG_NANOKONTROL2
+#define MIDIH_TEST_KORG_NANOKONTROL2 0
+#endif
+
+#define MIDIH_TEST_KORG_NANOKONTROL2_COUNT 5
+//--------------------------------------------------------------------+
+// STATIC GLOBALS DECLARATION
+//--------------------------------------------------------------------+
 static uint8_t midi_dev_addr = 0;
+static uint8_t first_note = 0x5b; // Mackie Control rewind
+static uint8_t last_note = 0x5f; // Mackie Control stop
+static uint8_t message[6] =
+{
+  0x90, 0x5f, 0x00,
+  0x90, 0x5b, 0x7f,
+};
+#if MIDIH_TEST_KORG_NANOKONTROL2
+// This command will dump a scene from a Kork nanoKONTROL2 control surface
+// Use it to test receiving long sysex messages
+static uint8_t nanoKONTROL2_dump_req[] = 
+{
+  0xf0, 0x42, 0x40, 0x00, 0x01, 0x13, 0x00, 0x1f, 0x10, 0x00, 0xf7
+};
+// wait MIDIH_TEST_KORG_NANOKONTROL2_COUNT seconds before requesting a dump
+static uint8_t dump_req_countdown = MIDIH_TEST_KORG_NANOKONTROL2_COUNT; 
+#endif
 
 static void test_tx(void)
 {
   // toggle NOTE On, Note Off for the Mackie Control channels 1-8 REC LED
   const uint32_t interval_ms = 1000;
   static uint32_t start_ms = 0;
-
-  static uint8_t message[] =
-  {
-    0x90, 0x00, 0x7f,
-    0x90, 0x01, 0x7f,
-    0x90, 0x02, 0x7f,
-    0x90, 0x03, 0x7f,
-    0x90, 0x04, 0x7f,
-    0x90, 0x05, 0x7f,
-    0x90, 0x06, 0x7f,
-    0x90, 0x07, 0x7f,
-  };
 
   // device must be attached and have at least one endpoint ready to receive a message
   if (!midi_dev_addr || !tuh_midi_configured(midi_dev_addr))
@@ -68,39 +93,45 @@ static void test_tx(void)
   }
   start_ms += interval_ms;
 
-  uint32_t nwritten = tuh_midi_stream_write(midi_dev_addr, 0, message, sizeof(message));
- 
-  char off_on[4] = {'O','n','\0'};
-  if (nwritten != 0)
+  uint32_t nwritten = 0;
+#if MIDIH_TEST_KORG_NANOKONTROL2
+  if (dump_req_countdown == 0)
   {
-    if (message[2] == 0x7f)
+    nwritten = tuh_midi_stream_write(midi_dev_addr, 0, nanoKONTROL2_dump_req, sizeof(nanoKONTROL2_dump_req));
+    if (nwritten != sizeof(nanoKONTROL2_dump_req))
     {
-      off_on[1] = 'n';
-      off_on[2] = '\0';
-      message[2] = 0;
-      message[5] = 0;
-      message[8] = 0;
-      message[11] = 0;
-      message[14] = 0;
-      message[17] = 0;
-      message[20] = 0;
-      message[23] = 0;
+      _MESS_FAILED();
+      TU_BREAKPOINT();
     }
-    else
-    {
-      off_on[1] = 'f';
-      off_on[2] = 'f';
-      off_on[3] = '\0';
-      message[2] = 0x7f;
-      message[5] = 0x7f;
-      message[8] = 0x7f;
-      message[11] = 0x7f;
-      message[14] = 0x7f;
-      message[17] = 0x7f;
-      message[20] = 0x7f;
-      message[23] = 0x7f;
-    }
-    TU_LOG1("Switched lights %s\r\n", off_on);
+    dump_req_countdown = MIDIH_TEST_KORG_NANOKONTROL2_COUNT;
+  }
+  else
+  {
+    nwritten = 0;
+    nwritten += tuh_midi_stream_write(midi_dev_addr, 0, message, sizeof(message));
+    --dump_req_countdown;
+  }
+#else
+  uint8_t cable = tuh_midih_get_num_tx_cables(midi_dev_addr) - 1;
+  nwritten = 0;
+  /// @bug For some reason, Arturia Keylab-88 ignores these messages if
+  /// they are transimitted on cable 0 first
+  //for (cable = 0; cable < tuh_midih_get_num_tx_cables(midi_dev_addr); cable++)
+    nwritten += tuh_midi_stream_write(midi_dev_addr, cable, message, sizeof(message));
+#endif
+ 
+#if MIDIH_TEST_KORG_NANOKONTROL2
+  if (nwritten != 0 && dump_req_countdown != MIDIH_TEST_KORG_NANOKONTROL2_COUNT)
+#else
+  if (nwritten != 0)
+#endif
+  {
+    ++message[1];
+    ++message[4];
+    if (message[1] > last_note)
+      message[1] = first_note;
+    if (message[4] > last_note)
+      message[4] = first_note;
   }
 }
 
@@ -138,6 +169,11 @@ void tuh_midi_mount_cb(uint8_t dev_addr, uint8_t in_ep, uint8_t out_ep, uint8_t 
 {
   printf("MIDI device address = %u, IN endpoint %u has %u cables, OUT endpoint %u has %u cables\r\n",
       dev_addr, in_ep & 0xf, num_cables_rx, out_ep & 0xf, num_cables_tx);
+  message[1] = last_note;
+  message[4] = first_note;
+#if MIDIH_TEST_KORG_NANOKONTROL2
+  dump_req_countdown = MIDIH_TEST_KORG_NANOKONTROL2_COUNT;
+#endif
   midi_dev_addr = dev_addr;
 }
 
