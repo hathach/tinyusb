388dc9b93ead5880b6f6ccf03c71aa058d9fb577
Phil <pscull@gmail.com>
2022-02-04 22:28:10 +0000

    HID micro parser

--- test/test/class/hid/test_hid_rip.c
+++ test/test/class/hid/test_hid_rip.c
@@ -81,3 +81,41 @@ void test_usage_with_page(void)
   TEST_ASSERT_EQUAL(0x00010002, pstate.usages[0]);
 }
 
+void test_globals_recorded(void) 
+{
+  uint8_t tb[] = { 
+    0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
+    0x15, 0x55,        // Logical Minimum 55
+  };
+  
+  tuh_hid_rip_state_t pstate;
+  hidrip_init_state(&pstate, (uint8_t*)&tb, sizeof(tb));
+  TEST_ASSERT_EQUAL(2, hidrip_next_item(&pstate));
+  TEST_ASSERT_EQUAL(2, hidrip_next_item(&pstate));
+  TEST_ASSERT_EQUAL(0, pstate.stack_index);
+  TEST_ASSERT_EQUAL(0x01, hidri_short_udata32(pstate.global_items[pstate.stack_index][0]));
+  TEST_ASSERT_EQUAL(0x55, hidri_short_data32(pstate.global_items[pstate.stack_index][1]));
+}
+
+void test_main_clears_local(void) 
+{
+  uint8_t tb[] = { 
+    0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
+    0x09, 0x02,        // Usage (Mouse)
+    0x39, 0x04,        // Designator Index 4
+    0xA1, 0x01,        // Collection (Application)
+    0x75, 0x05,        // REPORT_SIZE (5)    
+  };
+  
+  tuh_hid_rip_state_t pstate;
+  hidrip_init_state(&pstate, (uint8_t*)&tb, sizeof(tb));
+  TEST_ASSERT_EQUAL(2, hidrip_next_item(&pstate));
+  TEST_ASSERT_EQUAL(2, hidrip_next_item(&pstate));
+  TEST_ASSERT_EQUAL(2, hidrip_next_item(&pstate));
+  TEST_ASSERT_EQUAL(2, hidrip_next_item(&pstate));
+  TEST_ASSERT_EQUAL(1, pstate.usage_count);
+  TEST_ASSERT_EQUAL(4, hidri_short_data32(pstate.global_items[pstate.stack_index][3]));
+  TEST_ASSERT_EQUAL(2, hidrip_next_item(&pstate));
+  TEST_ASSERT_EQUAL(0, pstate.global_items[pstate.stack_index][3]);
+  TEST_ASSERT_EQUAL(0, pstate.usage_count);
+}
