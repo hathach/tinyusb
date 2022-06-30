eb1dab25f595ce4d0523a070cc4e10ef0c9ddd34
graham sanderson <graham.sanderson@raspberrypi.com>
2022-06-29 20:04:12 -0500

    comment out example.cmake warnings which break builds of rp2040 sdk or older compilers

--- examples/example.cmake
+++ examples/example.cmake
@@ -1,31 +1,32 @@
-target_compile_options(${PROJECT} PUBLIC
-        -Wall
-        -Wextra
-        -Werror
-        -Wfatal-errors
-        -Wdouble-promotion
-        #-Wstrict-prototypes
-        -Wstrict-overflow
-        #-Werror-implicit-function-declaration
-        -Wfloat-equal
-        #-Wundef
-        -Wshadow
-        -Wwrite-strings
-        -Wsign-compare
-        -Wmissing-format-attribute
-        -Wunreachable-code
-        -Wcast-align
-        -Wcast-function-type
-        -Wcast-qual
-        -Wnull-dereference
-        -Wuninitialized
-        -Wunused
-        -Wredundant-decls
-        )
-
-# GCC version 9 or prior has a bug with incorrect Wconversion warnings
-if (CMAKE_C_COMPILER_VERSION VERSION_GREATER_EQUAL 10.0)
-  target_compile_options(${PROJECT} PUBLIC
-        -Wconversion
-        )
-endif()
+# commented out for now as it breaks compilation with older GCC
+#target_compile_options(${PROJECT} PUBLIC
+#        -Wall
+#        -Wextra
+#        -Werror
+#        -Wfatal-errors
+#        -Wdouble-promotion
+#        #-Wstrict-prototypes
+#        -Wstrict-overflow
+#        #-Werror-implicit-function-declaration
+#        -Wfloat-equal
+#        #-Wundef
+#        -Wshadow
+#        -Wwrite-strings
+#        -Wsign-compare
+#        -Wmissing-format-attribute
+#        -Wunreachable-code
+#        -Wcast-align
+#        -Wcast-function-type
+#        -Wcast-qual
+#        -Wnull-dereference
+#        -Wuninitialized
+#        -Wunused
+#        -Wredundant-decls
+#        )
+#
+## GCC version 9 or prior has a bug with incorrect Wconversion warnings
+#if (CMAKE_C_COMPILER_VERSION VERSION_GREATER_EQUAL 10.0)
+#  target_compile_options(${PROJECT} PUBLIC
+#        -Wconversion
+#        )
+#endif()
