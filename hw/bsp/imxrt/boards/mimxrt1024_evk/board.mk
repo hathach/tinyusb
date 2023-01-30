11384159e1137860f197fd8f0232c259a1cc3820
hathach <thach@tinyusb.org>
2023-01-30 10:16:57 +0700

    suppress rt1024 warnings and fix typo

--- hw/bsp/imxrt/boards/mimxrt1024_evk/board.mk
+++ hw/bsp/imxrt/boards/mimxrt1024_evk/board.mk
@@ -1,6 +1,9 @@
 CFLAGS += -DCPU_MIMXRT1024DAG5A
 MCU_VARIANT = MIMXRT1024
 
+# warnings caused by mcu driver
+CFLAGS += -Wno-error=array-bounds
+
 # For flash-jlink target
 JLINK_DEVICE = MIMXRT1024DAG5A
 
