2a10d5c20bf3dd504f51b010321fc6b72bf1bb35
hathach <thach@tinyusb.org>
2023-03-16 11:39:53 +0700

    rename ra  board name

--- hw/bsp/ra/boards/ra4m3_ek/board.mk
+++ hw/bsp/ra/boards/ra4m3_ek/board.mk
@@ -8,7 +8,7 @@ FSP_MCU_DIR = hw/mcu/renesas/fsp/ra/fsp/src/bsp/mcu/ra4m3
 FSP_BOARD_DIR = hw/mcu/renesas/fsp/ra/board/ra4m3_ek
 
 # All source paths should be relative to the top level.
-LD_FILE = $(BOARD_PATH)/ek_ra4m3.ld
+LD_FILE = $(BOARD_PATH)/ra4m3_ek.ld
 
 # For flash-jlink target
 JLINK_DEVICE = R7FA4M3AF
