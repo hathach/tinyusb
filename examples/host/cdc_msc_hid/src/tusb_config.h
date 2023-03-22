a97f39151ebdf5569ff4831902648c518821a25c
hathach <thach@tinyusb.org>
2023-03-22 08:33:30 +0700

    fine tune CFG_TUH_HID value

--- examples/host/cdc_msc_hid/src/tusb_config.h
+++ examples/host/cdc_msc_hid/src/tusb_config.h
@@ -97,7 +97,7 @@
 
 #define CFG_TUH_HUB                 1 // number of supported hubs
 #define CFG_TUH_CDC                 1
-#define CFG_TUH_HID                 (4*CFG_TUH_DEVICE_MAX) // typical keyboard + mouse device can have 3-4 HID interfaces
+#define CFG_TUH_HID                 (3*CFG_TUH_DEVICE_MAX) // typical keyboard + mouse device can have 3-4 HID interfaces
 #define CFG_TUH_MSC                 1
 #define CFG_TUH_VENDOR              0
 
