5ef260d5fceeef49c8d1eb08fe39a12712b1ae53
hathach <thach@tinyusb.org>
2023-04-03 17:44:05 +0700

    fix build for esp32

--- hw/bsp/espressif/boards/adafruit_feather_esp32s2/board.cmake
+++ hw/bsp/espressif/boards/adafruit_feather_esp32s2/board.cmake
@@ -1,3 +1,2 @@
 # Apply board specific content here
 set(IDF_TARGET "esp32s2")
-set(FAMILY_MCUS ESP32S2)
--- hw/bsp/espressif/boards/espressif_addax_1/board.cmake
+++ hw/bsp/espressif/boards/espressif_addax_1/board.cmake
@@ -1,3 +1,2 @@
 # Apply board specific content here
 set(IDF_TARGET "esp32s3")
-set(FAMILY_MCUS ESP32S3)
