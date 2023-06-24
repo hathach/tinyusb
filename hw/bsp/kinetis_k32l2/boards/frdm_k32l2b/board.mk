b59b65d2ed0f6fa14b18b7bc233309d9b9710898
hathach <thach@tinyusb.org>
2023-06-24 18:26:51 +0700

    rename to kinetis_k32l2, rename OPT_MCU_KINETIS_K32 to OPT_MCU_KINETIS_K32L

--- hw/bsp/kinetis_k32l2/boards/frdm_k32l2b/board.mk
+++ hw/bsp/kinetis_k32l2/boards/frdm_k32l2b/board.mk
@@ -1,20 +1,15 @@
 MCU = K32L2B31A
 
-# This board uses TinyUF2 for updates
-UF2_FAMILY_ID = 0x7f83e793
-
-CFLAGS += \
-  -mcpu=cortex-m0plus \
-  -DCPU_K32L2B31VLH0A \
+CFLAGS += -DCPU_K32L2B31VLH0A
 
 # mcu driver cause following warnings
 CFLAGS += -Wno-error=unused-parameter -Wno-error=redundant-decls
 
 # All source paths should be relative to the top level.
-LD_FILE = $(BOARD_PATH)/K32L2B31xxxxA_flash.ld
+LD_FILE = $(MCU_DIR)/gcc/K32L2B31xxxxA_flash.ld
 
-# For freeRTOS port source
-FREERTOS_PORTABLE_SRC = $(FREERTOS_PORTABLE_PATH)/ARM_CM0
+SRC_C += \
+	$(MCU_DIR)/project_template/clock_config.c \
 
 # For flash-jlink target
 JLINK_DEVICE = K32L2B31xxxxA
--- hw/bsp/kinetis_k32l2/boards/kuiic/board.mk
+++ hw/bsp/kinetis_k32l2/boards/kuiic/board.mk
@@ -1,20 +1,12 @@
 MCU = K32L2B31A
 
-CFLAGS += \
-  -mcpu=cortex-m0plus \
-  -DCPU_K32L2B31VLH0A \
+CFLAGS += -DCPU_K32L2B31VLH0A
 
 # mcu driver cause following warnings
 CFLAGS += -Wno-error=unused-parameter -Wno-error=redundant-decls
 
 # All source paths should be relative to the top level.
-LD_FILE = $(MCU_DIR)/gcc/K32L2B31xxxxA_flash.ld
-
-SRC_C += \
-	$(MCU_DIR)/project_template/clock_config.c \
-
-# For freeRTOS port source
-FREERTOS_PORTABLE_SRC = $(FREERTOS_PORTABLE_PATH)/ARM_CM0
+LD_FILE = $(BOARD_PATH)/K32L2B31xxxxA_flash.ld
 
 # For flash-jlink target
 JLINK_DEVICE = K32L2B31xxxxA
