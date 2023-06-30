3cb4d738996e0856ab42654e421fd766652519bd
hathach <thach@tinyusb.org>
2023-06-30 14:52:04 +0700

    clean up ra makefile

--- hw/bsp/ra/boards/ra4m1_ek/board.mk
+++ hw/bsp/ra/boards/ra4m1_ek/board.mk
@@ -1,7 +1,14 @@
 CPU_CORE = cortex-m4
+MCU_VARIANT = ra4m1
 
-FSP_MCU_DIR = hw/mcu/renesas/fsp/ra/fsp/src/bsp/mcu/ra4m1
 FSP_BOARD_DIR = hw/mcu/renesas/fsp/ra/board/ra4m1_ek
+
+SRC_C += \
+	$(FSP_BOARD_DIR)/board_init.c \
+	$(FSP_BOARD_DIR)/board_leds.c \
+
+INC += \
+	$(TOP)/$(FSP_BOARD_DIR)
 
 # All source paths should be relative to the top level.
 LD_FILE = $(BOARD_PATH)/ra4m1_ek.ld
--- hw/bsp/ra/boards/ra4m3_ek/board.mk
+++ hw/bsp/ra/boards/ra4m3_ek/board.mk
@@ -1,7 +1,14 @@
 CPU_CORE = cortex-m33
+MCU_VARIANT = ra4m3
 
-FSP_MCU_DIR = hw/mcu/renesas/fsp/ra/fsp/src/bsp/mcu/ra4m3
 FSP_BOARD_DIR = hw/mcu/renesas/fsp/ra/board/ra4m3_ek
+
+SRC_C += \
+	$(FSP_BOARD_DIR)/board_init.c \
+	$(FSP_BOARD_DIR)/board_leds.c \
+
+INC += \
+	$(TOP)/$(FSP_BOARD_DIR)
 
 # All source paths should be relative to the top level.
 LD_FILE = $(BOARD_PATH)/ra4m3_ek.ld
