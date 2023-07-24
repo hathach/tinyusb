f295aaf1851eb270070af7292d1f100f7172daf4
hathach <thach@tinyusb.org>
2023-07-24 11:55:54 +0700

    add hcd abort xfer for rp2040 pio usb

--- src/portable/raspberrypi/pio_usb/hcd_pio_usb.c
+++ src/portable/raspberrypi/pio_usb/hcd_pio_usb.c
@@ -139,11 +139,8 @@ bool hcd_edpt_xfer(uint8_t rhport, uint8_t dev_addr, uint8_t ep_addr, uint8_t * 
 }
 
 bool hcd_edpt_abort_xfer(uint8_t rhport, uint8_t dev_addr, uint8_t ep_addr) {
-  (void) rhport;
-  (void) dev_addr;
-  (void) ep_addr;
-  // TODO not implemented yet
-  return false;
+  uint8_t const pio_rhport = RHPORT_PIO(rhport);
+  return pio_usb_host_endpoint_abort_transfer(pio_rhport, dev_addr, ep_addr);
 }
 
 bool hcd_setup_send(uint8_t rhport, uint8_t dev_addr, uint8_t const setup_packet[8])
