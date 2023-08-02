cfe8ca3fa51061bbe7e519b72236f9aabae13e59
Xelus22 <17491233+Xelus22@users.noreply.github.com>
2023-08-02 20:33:16 +1000

    Update usbd.c

--- src/device/usbd.c
+++ src/device/usbd.c
@@ -597,6 +597,16 @@ void tud_task_ext(uint32_t timeout_ms, bool in_isr)
       break;
 
       case DCD_EVENT_SOF:
+        TU_LOG_USBD("\r\n");
+        for (uint8_t i = 0; i < TOTAL_DRIVER_COUNT; i++)
+        {
+          usbd_class_driver_t const * driver = get_driver(i);
+          if (driver && driver->sof)
+          {
+            driver->sof(event.rhport, event.sof.frame_count);
+          }
+        }
+        break;
       default:
         TU_BREAKPOINT();
       break;
