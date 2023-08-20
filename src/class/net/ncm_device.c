e4c18c101cf5dc78f6bd1034aae7f0f7e22b7e47
Hardy Griech <ntbox@gmx.net>
2023-08-20 18:22:07 +0200

    Copyright

--- src/class/net/ncm_device.c
+++ src/class/net/ncm_device.c
@@ -2,6 +2,7 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2023 Hardy Griech
+ * Copyright (c) 2019 Ha Thach (tinyusb.org)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
