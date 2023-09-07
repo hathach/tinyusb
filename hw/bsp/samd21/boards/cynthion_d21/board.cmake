d3c08ecc3ab53b5e1b4fbcb18eb2aab78a5b840e
hathach <thach@tinyusb.org>
2023-09-07 17:20:07 +0700

    more ci fix

--- hw/bsp/samd21/boards/cynthion_d21/board.cmake
+++ hw/bsp/samd21/boards/cynthion_d21/board.cmake
@@ -1,5 +1,5 @@
 set(JLINK_DEVICE ATSAMD21G18)
-set(LD_FILE_GNU ${CMAKE_CURRENT_LIST_DIR}/${BOARD}.ld)
+set(LD_FILE_GNU ${CMAKE_CURRENT_LIST_DIR}/samd21g18a_flash.ld)
 
 function(update_board TARGET)
   target_compile_definitions(${TARGET} PUBLIC
