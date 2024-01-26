4c9f462423338a812378924233eac55ce0e96fb8
YixingShen <kkman3651878@hotmail.com>
2024-01-26 09:09:18 +0800

    add "set tx_busy 1" into video_capture's video_tas for "if (tx_busy) return"

--- examples/device/video_capture/src/main.c
+++ examples/device/video_capture/src/main.c
@@ -185,6 +185,7 @@ void video_task(void)
 
   if (!already_sent) {
     already_sent = 1;
+    tx_busy = 1;
     start_ms = board_millis();
 #ifdef CFG_EXAMPLE_VIDEO_READONLY
 # if defined(CFG_EXAMPLE_VIDEO_DISABLE_MJPEG)
@@ -203,6 +204,7 @@ void video_task(void)
   if (cur - start_ms < interval_ms) return; // not enough time
   if (tx_busy) return;
   start_ms += interval_ms;
+  tx_busy = 1;
 
 #ifdef CFG_EXAMPLE_VIDEO_READONLY
 # if defined(CFG_EXAMPLE_VIDEO_DISABLE_MJPEG)
