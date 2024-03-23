42c339dcb19c688c6736b1b9b6512f68c5e300ef
HiFiPhile <admin@hifiphile.com>
2024-03-23 18:03:08 +0100

    Improve example.

--- examples/device/uac2_speaker_fb/src/audio_debug.py
+++ examples/device/uac2_speaker_fb/src/audio_debug.py
@@ -30,8 +30,8 @@ if dev:
     fifo_cnt = []
     # This function is called periodically from FuncAnimation
     def animate(i):
-
-        for i in range(100):
+        info = None
+        for i in range(30):
             str_in = dev.read(64, 10)
             if str_in:
                 info = audio_debug_info_t.from_buffer_copy(str_in)
@@ -45,18 +45,19 @@ if dev:
         fifo_avg = fifo_avg[-1000:]
         fifo_cnt = fifo_cnt[-1000:]
 
-        # Draw x and y lists
-        ax.clear()
-        ax.plot(fifo_cnt, label='FIFO count')
-        ax.plot(fifo_avg, label='FIFO average')
-        ax.legend()
-        ax.set_ylim(bottom=0, top=info.fifo_size)
+        if info is not None:
+            # Draw x and y lists
+            ax.clear()
+            ax.plot(fifo_cnt, label='FIFO count')
+            ax.plot(fifo_avg, label='FIFO average')
+            ax.legend()
+            ax.set_ylim(bottom=0, top=info.fifo_size)
 
-        # Format plot
-        plt.title('FIFO information')
-        plt.grid()
+            # Format plot
+            plt.title('FIFO information')
+            plt.grid()
 
-        print(f'Sample rate:{info.sample_rate} | Alt settings:{info.alt_settings} | Volume:{info.volume[:]}')
+            print(f'Sample rate:{info.sample_rate} | Alt settings:{info.alt_settings} | Volume:{info.volume[:]}')
 
-    ani = animation.FuncAnimation(fig, animate, interval=100)
+    ani = animation.FuncAnimation(fig, animate, interval=10)
     plt.show()
