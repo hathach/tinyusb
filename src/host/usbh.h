cbbfbbb57ea8b4ea97ce4f2e2d157da9c25019fe
hathach <thach@tinyusb.org>
2024-04-08 13:16:08 +0700

    add cpuctl and pinctl to tuh_configure() option for max3421

--- src/host/usbh.h
+++ src/host/usbh.h
@@ -80,12 +80,16 @@ enum {
   TUH_CFGID_MAX3421 = 200,
 };
 
+typedef struct {
+  uint8_t max_nak; // max NAK per endpoint per frame
+  uint8_t cpuctl; // R16: CPU Control Register
+  uint8_t pinctl; // R17: Pin Control Register. FDUPSPI bit is ignored
+} tuh_configure_max3421_t;
+
 typedef union {
   // For TUH_CFGID_RPI_PIO_USB_CONFIGURATION use pio_usb_configuration_t
 
-  struct {
-    uint8_t max_nak;
-  } max3421;
+  tuh_configure_max3421_t max3421;
 } tuh_configure_param_t;
 
 //--------------------------------------------------------------------+
