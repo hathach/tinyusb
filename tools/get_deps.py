f1940439e419e2a1c72a284158e098bca70f279a
hathach <thach@tinyusb.org>
2024-05-04 10:07:54 +0700

    fix get_deps.py for samd5x_e5x

--- tools/get_deps.py
+++ tools/get_deps.py
@@ -179,7 +179,7 @@ deps_optional = {
                     'lpc11 lpc13 lpc15 lpc17 lpc18 lpc40 lpc43'
                     'stm32f0 stm32f1 stm32f2 stm32f3 stm32f4 stm32f7 stm32g0 stm32g4 stm32h5'
                     'stm32h7 stm32l0 stm32l1 stm32l4 stm32l5 stm32u5 stm32wb'
-                    'sam3x samd11 samd21 samd51 same5x same7x saml2x samg'
+                    'sam3x samd11 samd21 samd51 samd5x_e5x same5x same7x saml2x samg'
                     'tm4c'],
     'lib/sct_neopixel': ['https://github.com/gsteiert/sct_neopixel.git',
                          'e73e04ca63495672d955f9268e003cffe168fcd8',
