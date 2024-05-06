63d5103f42f5da11e08566a107c85b358e3d11aa
HiFiPhile <admin@hifiphile.com>
2024-05-07 00:18:31 +0200

    Increase TCP_WND for better performance.

--- examples/device/net_lwip_webserver/src/lwipopts.h
+++ examples/device/net_lwip_webserver/src/lwipopts.h
@@ -48,8 +48,8 @@
 #define LWIP_IP_ACCEPT_UDP_PORT(p)      ((p) == PP_NTOHS(67))
 
 #define TCP_MSS                         (1500 /*mtu*/ - 20 /*iphdr*/ - 20 /*tcphhr*/)
-#define TCP_SND_BUF                     (2 * TCP_MSS)
-#define TCP_WND                         (TCP_MSS)
+#define TCP_SND_BUF                     (4 * TCP_MSS)
+#define TCP_WND                         (4 * TCP_MSS)
 
 #define ETHARP_SUPPORT_STATIC_ENTRIES   1
 
@@ -59,7 +59,7 @@
 
 #define LWIP_SINGLE_NETIF               1
 
-#define PBUF_POOL_SIZE                  2
+#define PBUF_POOL_SIZE                  4
 
 #define HTTPD_USE_CUSTOM_FSDATA         0
 
