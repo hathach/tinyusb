7d03bb7ed9924d54f72c1a03a56704ea9f9b9ecf
hathach <thach@tinyusb.org>
2024-05-07 11:54:57 +0700

    more ci

--- tools/ci.py
+++ tools/ci.py
@@ -3,39 +3,39 @@ import json
 
 # family: toolchain
 family_list = {
-    "broadcom_32bit": ["armgcc"],
-    "imxrt": ["armgcc", "armclang"],
-    "kinetis_k kinetis_kl kinetis_k32l2": ["armgcc", "armclang"],
-    "lpc11 lpc13 lpc15": ["armgcc"],
-    "lpc17 lpc18 lpc40 lpc43": ["armgcc", "armclang"],
-    "lpc51 lpc54 lpc55": ["armgcc", "armclang"],
-    "mcx": ["armgcc"],
-    "mm32": ["armgcc"],
-    "msp432e4": ["armgcc"],
-    "nrf": ["armgcc", "armclang"],
-    "ra": ["armgcc"],
-    "rp2040": ["armgcc"],
-    "samd11 samd21 saml2x samd5x_e5x samg": ["armgcc", "armclang"],
-    "stm32f0": ["armgcc", "armclang"],
-    "stm32f1": ["armgcc", "armclang"],
-    "stm32f2": ["armgcc", "armclang"],
-    "stm32f3": ["armgcc", "armclang"],
-    "stm32f4": ["armgcc", "armclang"],
-    "stm32f7": ["armgcc", "armclang"],
-    "stm32g0": ["armgcc", "armclang"],
-    "stm32g4": ["armgcc", "armclang"],
-    "stm32h5": ["armgcc", "armclang"],
-    "stm32h7": ["armgcc", "armclang"],
-    "stm32l4": ["armgcc", "armclang"],
-    "stm32u5": ["armgcc", "armclang"],
-    "stm32wb": ["armgcc"],
-    "tm4c": ["armgcc"],
-    "xmc4000": ["armgcc"],
+    "broadcom_32bit": ["arm-gcc"],
+    "imxrt": ["arm-gcc", "arm-clang"],
+    "kinetis_k kinetis_kl kinetis_k32l2": ["arm-gcc", "arm-clang"],
+    "lpc11 lpc13 lpc15": ["arm-gcc"],
+    "lpc17 lpc18 lpc40 lpc43": ["arm-gcc", "arm-clang"],
+    "lpc51 lpc54 lpc55": ["arm-gcc", "arm-clang"],
+    "mcx": ["arm-gcc"],
+    "mm32": ["arm-gcc"],
+    "msp432e4": ["arm-gcc"],
+    "nrf": ["arm-gcc", "arm-clang"],
+    "ra": ["arm-gcc"],
+    "rp2040": ["arm-gcc"],
+    "samd11 samd21 saml2x samd5x_e5x samg": ["arm-gcc", "arm-clang"],
+    "stm32f0": ["arm-gcc", "arm-clang"],
+    "stm32f1": ["arm-gcc", "arm-clang"],
+    "stm32f2": ["arm-gcc", "arm-clang"],
+    "stm32f3": ["arm-gcc", "arm-clang"],
+    "stm32f4": ["arm-gcc", "arm-clang"],
+    "stm32f7": ["arm-gcc", "arm-clang"],
+    "stm32g0": ["arm-gcc", "arm-clang"],
+    "stm32g4": ["arm-gcc", "arm-clang"],
+    "stm32h5": ["arm-gcc", "arm-clang"],
+    "stm32h7": ["arm-gcc", "arm-clang"],
+    "stm32l4": ["arm-gcc", "arm-clang"],
+    "stm32u5": ["arm-gcc", "arm-clang"],
+    "stm32wb": ["arm-gcc"],
+    "tm4c": ["arm-gcc"],
+    "xmc4000": ["arm-gcc"],
 }
 
 
 @click.command()
-@click.option('--toolchain', default='armgcc', help='Toolchain to filter by.')
+@click.option('--toolchain', default='arm-gcc', help='Toolchain to filter by.')
 def get_family_matrix(toolchain):
     filtered_families = [family for family, toolchains in family_list.items() if toolchain in toolchains]
     print(json.dumps({"family": filtered_families}, indent=2))
