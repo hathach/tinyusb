d432ed41f461d3ecc0901585b8e06804c66d815d
hathach <thach@tinyusb.org>
2024-05-09 12:20:26 +0700

    build.py support esp32 board

--- tools/build.py
+++ tools/build.py
@@ -3,6 +3,7 @@ import sys
 import time
 import subprocess
 import click
+from pathlib import Path
 from multiprocessing import Pool
 
 import build_utils
@@ -13,35 +14,11 @@ FAILED = "\033[31mfailed\033[0m"
 build_separator = '-' * 106
 
 
-def build_board_cmake(board, toolchain):
-    start_time = time.monotonic()
-    build_dir = f"cmake-build/cmake-build-{board}"
-
-    # Generate build
-    r = subprocess.run(f"cmake examples -B {build_dir} -G \"Ninja\" -DBOARD={board} -DCMAKE_BUILD_TYPE=MinSizeRel " \
-                       f"-DTOOLCHAIN={toolchain}", shell=True, stdout=subprocess.PIPE,
-                       stderr=subprocess.STDOUT)
-
-    # Build
-    if r.returncode == 0:
-        r = subprocess.run(f"cmake --build {build_dir}", shell=True, stdout=subprocess.PIPE,
-                           stderr=subprocess.STDOUT)
-
-    duration = time.monotonic() - start_time
-
-    if r.returncode == 0:
-        status = SUCCEEDED
-    else:
-        status = FAILED
-
-    flash_size = "-"
-    sram_size = "-"
-    example = 'all'
-    title = build_utils.build_format.format(example, board, status, "{:.2f}s".format(duration), flash_size, sram_size)
-
-    if r.returncode == 0:
-        print(title)
-    else:
+def run_cmd(cmd):
+    #print(cmd)
+    r = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    title = 'command error'
+    if r.returncode != 0:
         # print build output if failed
         if os.getenv('CI'):
             print(f"::group::{title}")
@@ -50,19 +27,74 @@ def build_board_cmake(board, toolchain):
         else:
             print(title)
             print(r.stdout.decode("utf-8"))
+    return r
 
-    return r.returncode == 0
+def find_family(board):
+    bsp_dir = Path("hw/bsp")
+    for family_dir in bsp_dir.iterdir():
+        if family_dir.is_dir():
+            board_dir = family_dir / 'boards' / board
+            if board_dir.exists():
+                return family_dir.name
+    return None
 
 
-def get_examples_make():
+def get_examples(family):
     all_examples = []
     for d in os.scandir("examples"):
         if d.is_dir() and 'cmake' not in d.name and 'build_system' not in d.name:
             for entry in os.scandir(d.path):
                 if entry.is_dir() and 'cmake' not in entry.name:
-                    all_examples.append(d.name + '/' + entry.name)
+                    if family != 'espressif' or 'freertos' in entry.name:
+                        all_examples.append(d.name + '/' + entry.name)
+
+    if family == 'espressif':
+        all_examples.append('device/board_test')
+        all_examples.append('device/video_capture')
     all_examples.sort()
     return all_examples
+
+
+def build_board_cmake(board, toolchain):
+    start_time = time.monotonic()
+    ret = [0, 0, 0]
+
+    build_dir = f"cmake-build/cmake-build-{board}"
+    family = find_family(board)
+    if family == 'espressif':
+        # for espressif, we have to build example individually
+        all_examples = get_examples(family)
+        for example in all_examples:
+            r = run_cmd(f'cmake examples/{example} -B {build_dir}/{example} -G "Ninja" -DBOARD={board} -DMAX3421_HOST=1')
+            if r.returncode == 0:
+                r = run_cmd(f'cmake --build {build_dir}/{example}')
+            if r.returncode == 0:
+                ret[0] += 1
+            else:
+                ret[1] += 1
+    else:
+        r = run_cmd(f'cmake examples -B {build_dir} -G "Ninja" -DBOARD={board} -DCMAKE_BUILD_TYPE=MinSizeRel -DTOOLCHAIN={toolchain}')
+        if r.returncode == 0:
+            r = run_cmd(f"cmake --build {build_dir}")
+        if r.returncode == 0:
+            ret[0] += 1
+        else:
+            ret[1] += 1
+
+    duration = time.monotonic() - start_time
+
+    if ret[1] == 0:
+        status = SUCCEEDED
+    else:
+        status = FAILED
+
+    flash_size = "-"
+    sram_size = "-"
+    example = 'all'
+    title = build_utils.build_format.format(example, board, status, "{:.2f}s".format(duration), flash_size, sram_size)
+    print(title)
+    return ret
+
 
 def build_family(family, toolchain, build_system):
     all_boards = []
@@ -80,7 +112,7 @@ def build_family(family, toolchain, build_system):
             else:
                 ret[1] += 1
     elif build_system == 'make':
-        all_examples = get_examples_make()
+        all_examples = get_examples(family)
         for example in all_examples:
             with Pool(processes=os.cpu_count()) as pool:
                 pool_args = list((map(lambda b, e=example, o=f"TOOLCHAIN={toolchain}": [e, b, o], all_boards)))
@@ -94,13 +126,13 @@ def build_family(family, toolchain, build_system):
 
 
 @click.command()
-@click.argument('family', nargs=-1, required=False)
+@click.argument('families', nargs=-1, required=False)
 @click.option('-b', '--board', multiple=True, default=None, help='Boards to build')
 @click.option('-t', '--toolchain', default='gcc', help='Toolchain to use, default is gcc')
 @click.option('-s', '--build-system', default='cmake', help='Build system to use, default is cmake')
-def main(family, board, toolchain, build_system):
-    if len(family) == 0 and len(board) == 0:
-        print("Please specify family or board to build")
+def main(families, board, toolchain, build_system):
+    if len(families) == 0 and len(board) == 0:
+        print("Please specify families or board to build")
         return 1
 
     print(build_separator)
@@ -109,14 +141,14 @@ def main(family, board, toolchain, build_system):
     total_result = [0, 0, 0]
 
     # build families: cmake, make
-    if family is not None:
+    if families is not None:
         all_families = []
-        if 'all' in family:
+        if 'all' in families:
             for entry in os.scandir("hw/bsp"):
                 if entry.is_dir() and entry.name != 'espressif' and os.path.isfile(entry.path + "/family.cmake"):
                     all_families.append(entry.name)
         else:
-            all_families = list(family)
+            all_families = list(families)
         all_families.sort()
 
         # succeeded, failed
@@ -129,10 +161,10 @@ def main(family, board, toolchain, build_system):
     # build board (only cmake)
     if board is not None:
         for b in board:
-            if build_board_cmake(b, toolchain):
-                total_result[0] += 1
-            else:
-                total_result[1] += 1
+            r = build_board_cmake(b, toolchain)
+            total_result[0] += r[0]
+            total_result[1] += r[1]
+            total_result[2] += r[2]
 
     total_time = time.monotonic() - total_time
     print(build_separator)
