301fb2a9f77a50c806c2147946eb434e360be8b0
HiFiPhile <admin@hifiphile.com>
2024-05-12 14:05:57 +0200

    Fix CI.

--- examples/device/uac2_speaker_fb/src/main.c
+++ examples/device/uac2_speaker_fb/src/main.c
@@ -373,6 +373,7 @@ void tud_audio_feedback_params_cb(uint8_t func_id, uint8_t alt_itf, audio_feedba
 bool tud_audio_rx_done_pre_read_cb(uint8_t rhport, uint16_t n_bytes_received, uint8_t func_id, uint8_t ep_out, uint8_t cur_alt_setting)
 {
   (void)rhport;
+  (void)n_bytes_received;
   (void)func_id;
   (void)ep_out;
   (void)cur_alt_setting;
