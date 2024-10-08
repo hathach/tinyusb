07c14f30a6d6cfb1758e3496c3ea73275ae4a062
hathach <thach@tinyusb.org>
2024-10-08 12:36:45 +0700

    add f407disco to hil pool

--- hw/bsp/stm32h7/boards/stm32h750_weact/board.cmake
+++ hw/bsp/stm32h7/boards/stm32h750_weact/board.cmake
@@ -1,5 +1,5 @@
 set(MCU_VARIANT stm32h750xx)
-set(JLINK_DEVICE stm32h750xb)
+set(JLINK_DEVICE stm32h750vb)
 
 set(LD_FILE_GNU ${CMAKE_CURRENT_LIST_DIR}/${MCU_VARIANT}_flash_CM7.ld)
 set(LD_FILE_IAR ${ST_CMSIS}/Source/Templates/iar/linker/${MCU_VARIANT}_flash.icf)
