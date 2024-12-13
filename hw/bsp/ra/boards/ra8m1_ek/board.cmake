c79890c3597dabbe94340542a8c5c7cd168d8aa0
hathach <thach@tinyusb.org>
2024-12-13 12:50:51 +0700

    ra8m1_ek led and button works

---
+++ hw/bsp/ra/boards/ra8m1_ek/board.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_PROCESSOR cortex-m85 CACHE INTERNAL "System Processor")
+set(MCU_VARIANT ra8m1)
+
+set(JLINK_DEVICE R7FA8M1AH)
+#set(JLINK_OPTION "-USB 001083115236")
+
+function(update_board TARGET)
+endfunction()
