16010f2c6172c4e6983e69cad2ad2d7f683ba8a1
i-am-shodan <6901273+i-am-shodan@users.noreply.github.com>
2025-01-17 11:42:21 +0000

    fixed build issue

--- test/fuzz/msc_fuzz.cc
+++ test/fuzz/msc_fuzz.cc
@@ -160,6 +160,7 @@ int32_t tud_msc_scsi_cb(uint8_t lun, uint8_t const scsi_cmd[16], void *buffer,
 
 msc_scsi_device_type_t tud_msc_scsi_device_type(uint8_t lun)
 {
+  (void)lun;
   return MSC_SCSI_DEVICE_BLOCK;
 }
 }
