ffbbbc04e9dfdba05b7feb2afae8b3f3c62235ac
i-am-shodan <6901273+i-am-shodan@users.noreply.github.com>
2025-01-17 13:45:31 +0000

    yet another example fix

--- examples/device/msc_dual_lun/src/msc_disk_dual.c
+++ examples/device/msc_dual_lun/src/msc_disk_dual.c
@@ -333,6 +333,7 @@ int32_t tud_msc_scsi_cb(uint8_t lun, uint8_t const scsi_cmd[16], void* buffer, u
 
 msc_scsi_device_type_t tud_msc_scsi_device_type(uint8_t lun)
 {
+  (void)lun;
   return MSC_SCSI_DEVICE_BLOCK;
 }
 
