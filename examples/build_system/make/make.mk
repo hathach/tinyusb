hathach/tinyusb

commit a64e3eb0aa661db14eb647e2f75059815741112f

update board_test always output to uart regardless of LOGGER option

examples/build_system/make/make.mk
@@ -123,27 +123,24 @@ endif
 ifeq (${MAX3421_HOST},1)
   SRC_C += src/portable/analog/max3421/hcd_max3421.c
   CFLAGS += -DCFG_TUH_MAX3421=1
-  CMAKE_DEFSYM +=	-DMAX3421_HOST=1
 endif
 
 # Log level is mapped to TUSB DEBUG option
 ifneq ($(LOG),)
-  CMAKE_DEFSYM +=	-DLOG=$(LOG)
   CFLAGS += -DCFG_TUSB_DEBUG=$(LOG)
 endif
 
 # Logger: default is uart, can be set to rtt or swo
-ifneq ($(LOGGER),)
-  CMAKE_DEFSYM +=	-DLOGGER=$(LOGGER)
-endif
-
 ifeq ($(LOGGER),rtt)
-  CFLAGS += -DLOGGER_RTT -DSEGGER_RTT_MODE_DEFAULT=SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL
-  RTT_SRC = lib/SEGGER_RTT
-  INC   += $(TOP)/$(RTT_SRC)/RTT
-  SRC_C += $(RTT_SRC)/RTT/SEGGER_RTT.c
-else ifeq ($(LOGGER),swo)
+  CFLAGS += -DLOGGER_RTT
+  #CFLAGS += -DSEGGER_RTT_MODE_DEFAULT=SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL
+  INC   += $(TOP)/$(lib/SEGGER_RTT)/RTT
+  SRC_C += $(lib/SEGGER_RTT)/RTT/SEGGER_RTT.c
+endif
+ifeq ($(LOGGER),swo)
   CFLAGS += -DLOGGER_SWO
+else
+  CFLAGS += -DLOGGER_UART
 endif
 
 # CPU specific flags

test/fuzz/make.mk
@@ -124,11 +124,5 @@ endif
 
 # Log level is mapped to TUSB DEBUG option
 ifneq ($(LOG),)
-  CMAKE_DEFSYM +=	-DLOG=$(LOG)
   CFLAGS += -DCFG_TUSB_DEBUG=$(LOG)
-endif
-
-# Logger: default is uart, can be set to rtt or swo
-ifneq ($(LOGGER),)
-	CMAKE_DEFSYM +=	-DLOGGER=$(LOGGER)
 endif

examples/device/board_test/src/main.c
@@ -52,6 +52,9 @@ int main(void) {
     int ch = board_getchar();
     if (ch > 0) {
       board_putchar(ch);
+      #ifndef LOGGER_UART
+      board_uart_write(&ch, 1);
+      #endif
     }
 
     // Blink and print every interval ms
@@ -61,7 +64,10 @@ int main(void) {
       if (ch < 0) {
         // skip if echoing
         printf(HELLO_STR);
+
+        #ifndef LOGGER_UART
         board_uart_write(HELLO_STR, strlen(HELLO_STR));
+        #endif
       }
 
       board_led_write(led_state);

hw/bsp/board.c
@@ -60,7 +60,6 @@ int sys_read(int fhdl, char *buf, size_t count) {
   int rd = (int) SEGGER_RTT_Read(0, buf, count);
   return (rd > 0) ? rd : -1;
 }
-
 #endif
 
 #elif defined(LOGGER_SWO)

hw/bsp/family_support.cmake
@@ -221,6 +221,8 @@ function(family_configure_common TARGET RTOS)
       target_include_directories(${TARGET}  PUBLIC ${TOP}/lib/SEGGER_RTT/RTT)
 #      target_compile_definitions(${TARGET}  PUBLIC SEGGER_RTT_MODE_DEFAULT=SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL)
     endif ()
+  else ()
+    target_compile_definitions(${TARGET} PUBLIC LOGGER_UART)
   endif ()
 
   if (CMAKE_C_COMPILER_ID STREQUAL "GNU" OR CMAKE_C_COMPILER_ID STREQUAL "Clang")

commit 94b03e2cb4c22ef260e21568f04954f6f2c99326

examples: make: fix LOGGER=rtt
CMake builds worked, but the variables were brokenly updated for make.

Fixes: a64e3eb0aa update board_test always output on uart
Signed-off-by: Karl Palsson <karl.palsson@marel.com>

examples/build_system/make/make.mk
@@ -134,8 +134,8 @@ endif
 ifeq ($(LOGGER),rtt)
   CFLAGS += -DLOGGER_RTT
   #CFLAGS += -DSEGGER_RTT_MODE_DEFAULT=SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL
-  INC   += $(TOP)/$(lib/SEGGER_RTT)/RTT
-  SRC_C += $(lib/SEGGER_RTT)/RTT/SEGGER_RTT.c
+  INC   += $(TOP)/lib/SEGGER_RTT/RTT
+  SRC_C += lib/SEGGER_RTT/RTT/SEGGER_RTT.c
 endif
 ifeq ($(LOGGER),swo)
   CFLAGS += -DLOGGER_SWO