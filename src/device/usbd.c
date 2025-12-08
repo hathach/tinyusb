4e365fcef38cb2a024267f818b1c29ebb6e64ae3
Zixun LI <admin@hifiphile.com>
2025-12-08 11:48:57 +0100

    usbd: remove dcd_edpt_xfer_fifo ifdef guard
    Signed-off-by: Zixun LI <admin@hifiphile.com>

--- src/device/usbd.c
+++ src/device/usbd.c
@@ -1474,13 +1474,12 @@ bool usbd_edpt_xfer(uint8_t rhport, uint8_t ep_addr, uint8_t* buffer, uint16_t t
 // success message. If total_bytes is too big, the FIFO will copy only what is available
 // into the USB buffer!
 bool usbd_edpt_xfer_fifo(uint8_t rhport, uint8_t ep_addr, tu_fifo_t* ff, uint16_t total_bytes, bool is_isr) {
-  #if CFG_TUD_EDPT_DEDICATED_HWFIFO
   rhport = _usbd_rhport;
 
   uint8_t const epnum = tu_edpt_number(ep_addr);
   uint8_t const dir = tu_edpt_dir(ep_addr);
 
-  TU_LOG_USBD("  Queue ISO EP %02X with %u bytes ... ", ep_addr, total_bytes);
+  TU_LOG_USBD("  Queue FIFO EP %02X with %u bytes ... ", ep_addr, total_bytes);
 
   // Attempt to transfer on a busy endpoint, sound like a race condition !
   TU_ASSERT(_usbd_dev.ep_status[epnum][dir].busy == 0);
@@ -1500,14 +1499,6 @@ bool usbd_edpt_xfer_fifo(uint8_t rhport, uint8_t ep_addr, tu_fifo_t* ff, uint16_
     TU_BREAKPOINT();
     return false;
   }
-  #else
-  (void)rhport;
-  (void)ep_addr;
-  (void)ff;
-  (void)total_bytes;
-  (void)is_isr;
-  return false;
-  #endif
 }
 
 bool usbd_edpt_busy(uint8_t rhport, uint8_t ep_addr) {
