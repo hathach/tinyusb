e37991fe98f27185017fdecd40ec6d8f6766cc87
HiFiPhile <admin@hifiphile.com>
2025-12-11 19:29:18 +0100

    bsp: migrate lpc51, lpc55 to new sdk repo
    Signed-off-by: HiFiPhile <admin@hifiphile.com>

--- hw/bsp/lpc55/family.mk
+++ hw/bsp/lpc55/family.mk
@@ -1,9 +1,9 @@
 UF2_FAMILY_ID = 0x2abc77ec
-SDK_DIR = hw/mcu/nxp/mcux-sdk
 
 include $(TOP)/$(BOARD_PATH)/board.mk
 CPU_CORE ?= cortex-m33
-MCU_DIR = $(SDK_DIR)/devices/$(MCU_VARIANT)
+MCUX_DIR = /hw/mcu/nxp/mcuxsdk-core
+SDK_DIR = /hw/mcu/nxp/mcux-devices-lpc
 
 # Default to Highspeed PORT1
 PORT ?= 1
@@ -31,33 +31,36 @@ CFLAGS += -Wno-error=unused-parameter -Wno-error=float-equal
 LDFLAGS_GCC += \
   -nostartfiles \
   --specs=nosys.specs --specs=nano.specs \
+  -Wl,--defsym=__stack_size__=0x1000 \
+  -Wl,--defsym=__heap_size__=0 \
 
 # All source paths should be relative to the top level.
-LD_FILE ?= $(MCU_DIR)/gcc/$(MCU_CORE)_flash.ld
+LD_FILE ?= $(SDK_DIR)/LPC5500/$(MCU_VARIANT)/gcc/$(MCU_CORE)_flash.ld
 
 SRC_C += \
-	src/portable/nxp/lpc_ip3511/dcd_lpc_ip3511.c \
-	$(MCU_DIR)/system_$(MCU_CORE).c \
-	$(MCU_DIR)/drivers/fsl_clock.c \
-	$(MCU_DIR)/drivers/fsl_power.c \
-	$(MCU_DIR)/drivers/fsl_reset.c \
-	$(SDK_DIR)/drivers/lpc_gpio/fsl_gpio.c \
-	$(SDK_DIR)/drivers/common/fsl_common_arm.c \
-	$(SDK_DIR)/drivers/flexcomm/fsl_flexcomm.c \
-	$(SDK_DIR)/drivers/flexcomm/usart/fsl_usart.c \
-	lib/sct_neopixel/sct_neopixel.c
+	$(TOP)/src/portable/nxp/lpc_ip3511/dcd_lpc_ip3511.c \
+	$(TOP)/$(SDK_DIR)/LPC5500/$(MCU_VARIANT)/system_$(MCU_CORE).c \
+	$(TOP)/$(SDK_DIR)/LPC5500/$(MCU_VARIANT)/drivers/fsl_clock.c \
+	$(TOP)/$(SDK_DIR)/LPC5500/$(MCU_VARIANT)/drivers/fsl_power.c \
+	$(TOP)/$(SDK_DIR)/LPC5500/$(MCU_VARIANT)/drivers/fsl_reset.c \
+	$(TOP)/$(MCUX_DIR)/drivers/lpc_gpio/fsl_gpio.c \
+	$(TOP)/$(MCUX_DIR)/drivers/common/fsl_common_arm.c \
+	$(TOP)/$(MCUX_DIR)/drivers/flexcomm/fsl_flexcomm.c \
+	$(TOP)/$(MCUX_DIR)/drivers/flexcomm/usart/fsl_usart.c \
+	$(TOP)/lib/sct_neopixel/sct_neopixel.c
 
 INC += \
 	$(TOP)/$(BOARD_PATH) \
 	$(TOP)/lib/sct_neopixel \
-	$(TOP)/lib/CMSIS_5/CMSIS/Core/Include \
-	$(TOP)/$(MCU_DIR) \
-	$(TOP)/$(MCU_DIR)/drivers \
-	$(TOP)/$(SDK_DIR)/drivers/common \
-	$(TOP)/$(SDK_DIR)/drivers/flexcomm/usart \
-	$(TOP)/$(SDK_DIR)/drivers/flexcomm/ \
-	$(TOP)/$(SDK_DIR)/drivers/lpc_iocon \
-	$(TOP)/$(SDK_DIR)/drivers/lpc_gpio \
-	$(TOP)/$(SDK_DIR)/drivers/sctimer
+	$(TOP)/lib/CMSIS_6/CMSIS/Core/Include \
+	$(TOP)/$(SDK_DIR)/LPC5500/$(MCU_VARIANT) \
+	$(TOP)/$(SDK_DIR)/LPC5500/$(MCU_VARIANT)/drivers \
+	$(TOP)/$(SDK_DIR)/LPC5500/periph \
+	$(TOP)/$(MCUX_DIR)/drivers/common \
+	$(TOP)/$(MCUX_DIR)/drivers/flexcomm/usart \
+	$(TOP)/$(MCUX_DIR)/drivers/flexcomm/ \
+	$(TOP)/$(MCUX_DIR)/drivers/lpc_iocon \
+	$(TOP)/$(MCUX_DIR)/drivers/lpc_gpio \
+	$(TOP)/$(MCUX_DIR)/drivers/sctimer
 
-SRC_S += $(MCU_DIR)/gcc/startup_$(MCU_CORE).S
+SRC_S += $(TOP)/$(SDK_DIR)/LPC5500/$(MCU_VARIANT)/gcc/startup_$(MCU_CORE).S
