36daff5612d12acd4759b9493b102e145b4559d2
alt-0191 <2223147307@qq.com>
2026-02-26 21:13:07 +0800

    ci: add ch58x to build matrix (riscv-gcc)

--- .github/workflows/ci_set_matrix.py
+++ .github/workflows/ci_set_matrix.py
@@ -28,6 +28,7 @@ family_list = {
     "ch32v10x": ["riscv-gcc"],
     "ch32v20x": ["riscv-gcc"],
     "ch32v30x": ["riscv-gcc"],
+    "ch58x": ["riscv-gcc"],
     "da1469x": ["arm-gcc"],
     "fomu": ["riscv-gcc"],
     "gd32vf103": ["riscv-gcc"],
