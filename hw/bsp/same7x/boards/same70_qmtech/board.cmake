fc9f63e62d0806b233602cc1bb45b370d350a717
HiFiPhile <admin@hifiphile.com>
2026-02-27 10:43:22 +0100

    bsp/samx7x: fix build
    Signed-off-by: HiFiPhile <admin@hifiphile.com>

--- hw/bsp/same7x/boards/same70_qmtech/board.cmake
+++ hw/bsp/same7x/boards/same70_qmtech/board.cmake
@@ -1,5 +1,9 @@
 set(JLINK_DEVICE SAME70N19B)
-set(LD_FILE_GNU ${TOP}/hw/mcu/microchip/same70/same70b/gcc/gcc/same70q21b_flash.ld)
+set(LD_FILE_GNU ${TOP}/hw/mcu/microchip/same70/same70b/gcc/gcc/same70n19b_flash.ld)
+set(LD_FILE_IAR ${TOP}/hw/mcu/microchip/same70/same70b/iar/config/linker/Microchip/atsame70n19b/flash.icf)
+
+set(STARTUP_FILE_GNU ${TOP}/hw/mcu/microchip/same70/same70b/gcc/gcc/startup_same70n19b.c)
+set(STARTUP_FILE_IAR ${TOP}/hw/mcu/microchip/same70/same70b/iar/iar/startup_same70n19b.c)
 
 function(update_board TARGET)
   target_compile_definitions(${TARGET} PUBLIC
