94baf394686f92d9edaf109dcae30fa7b531fefc
hathach <thach@tinyusb.org>
2026-03-06 10:59:32 +0700

    fix warnings

--- hw/bsp/stm32wba/family.c
+++ hw/bsp/stm32wba/family.c
@@ -175,7 +175,10 @@ void board_init(void) {
   #endif // USB_OTG_HS
 }
 
-void board_led_write(bool state) { HAL_GPIO_WritePin(LED_PORT, LED_PIN, state ? LED_STATE_ON : (1 - LED_STATE_ON)); }
+void board_led_write(bool state) {
+  GPIO_PinState pin_state = (GPIO_PinState)(state ? LED_STATE_ON : (1 - LED_STATE_ON));
+  HAL_GPIO_WritePin(LED_PORT, LED_PIN, pin_state);
+}
 
 uint32_t board_button_read(void) { return HAL_GPIO_ReadPin(BUTTON_PORT, BUTTON_PIN) == BUTTON_STATE_ACTIVE; }
 
