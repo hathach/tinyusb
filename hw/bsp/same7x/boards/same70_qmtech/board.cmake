3a0227b7718529cf0397d66d2a7a5567e18498c3
HiFiPhile <admin@hifiphile.com>
2026-03-06 12:52:13 +0100

    fix Jlink device name
    Signed-off-by: HiFiPhile <admin@hifiphile.com>

--- hw/bsp/same7x/boards/same70_qmtech/board.cmake
+++ hw/bsp/same7x/boards/same70_qmtech/board.cmake
@@ -1,4 +1,4 @@
-set(JLINK_DEVICE SAME70N19B)
+set(JLINK_DEVICE ATSAME70N19B)
 set(LD_FILE_GNU ${TOP}/hw/mcu/microchip/same70/same70b/gcc/gcc/same70n19b_flash.ld)
 set(LD_FILE_IAR ${TOP}/hw/mcu/microchip/same70/same70b/iar/config/linker/Microchip/atsame70n19b/flash.icf)
 
