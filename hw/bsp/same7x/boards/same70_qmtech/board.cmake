6c4f119dddebc9af407f43f40fc1a7776efc7ed1
hathach <thach@tinyusb.org>
2026-03-07 13:01:32 +0700

    - update SAME70 BSP: remove unused definitions, consolidate startup files - and add unique ID - add freertos

--- hw/bsp/same7x/boards/same70_qmtech/board.cmake
+++ hw/bsp/same7x/boards/same70_qmtech/board.cmake
@@ -1,9 +1,8 @@
 set(JLINK_DEVICE ATSAME70N19B)
-set(LD_FILE_GNU ${TOP}/hw/mcu/microchip/same70/same70b/gcc/gcc/same70n19b_flash.ld)
-set(LD_FILE_IAR ${TOP}/hw/mcu/microchip/same70/same70b/iar/config/linker/Microchip/atsame70n19b/flash.icf)
+set(LD_FILE_GNU ${CMAKE_CURRENT_LIST_DIR}/same70n19b_flash.ld)
 
-set(STARTUP_FILE_GNU ${TOP}/hw/mcu/microchip/same70/same70b/gcc/gcc/startup_same70n19b.c)
-set(STARTUP_FILE_IAR ${TOP}/hw/mcu/microchip/same70/same70b/iar/iar/startup_same70n19b.c)
+# N19B and Q21B share the same vector table / startup code
+set(STARTUP_FILE_GNU ${TOP}/hw/mcu/microchip/same70/same70b/gcc/gcc/startup_same70q21b.c)
 
 function(update_board TARGET)
   target_compile_definitions(${TARGET} PUBLIC
