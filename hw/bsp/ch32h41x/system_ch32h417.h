d231a838b90a6ecdfe217d4a5e3405e49bcb5ba8
Jie Feng <hjf3108@gmail.com>
2026-05-18 11:19:00 +0800

    Fix pre-commit

--- hw/bsp/ch32h41x/system_ch32h417.h
+++ hw/bsp/ch32h41x/system_ch32h417.h
@@ -6,21 +6,21 @@
 * Description        : CH32H417 Device Peripheral Access Layer System Header File.
 *********************************************************************************
 * Copyright (c) 2025 Nanjing Qinheng Microelectronics Co., Ltd.
-* Attention: This software (modified or not) and binary are used for 
+* Attention: This software (modified or not) and binary are used for
 * microcontroller manufactured by Nanjing Qinheng Microelectronics.
 *******************************************************************************/
-#ifndef __SYSTEM_CH32H417_H 
+#ifndef __SYSTEM_CH32H417_H
 #define __SYSTEM_CH32H417_H
 
 #ifdef __cplusplus
  extern "C" {
-#endif 
+#endif
 
 extern uint32_t HCLKClock;
 extern uint32_t SystemClock;               /* System Clock Frequency */
 extern uint32_t SystemCoreClock;           /* System Core Frequency */
 
-/* System_Exported_Functions */  
+/* System_Exported_Functions */
 extern void SystemInit(void);
 extern void SystemAndCoreClockUpdate(void);
 
@@ -28,7 +28,4 @@ extern void SystemAndCoreClockUpdate(void);
 }
 #endif
 
-#endif 
-
-
-
+#endif
