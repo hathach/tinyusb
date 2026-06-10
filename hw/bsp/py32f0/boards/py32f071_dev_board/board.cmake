46a76f974baf0e9c80e15008e7d8859015719081
Jie Feng <hjf3108@gmail.com>
2026-06-10 18:49:07 +0800

    examples now build

--- hw/bsp/py32f0/boards/py32f071_dev_board/board.cmake
+++ hw/bsp/py32f0/boards/py32f071_dev_board/board.cmake
@@ -4,6 +4,7 @@ set(LD_FILE_GNU ${CMAKE_CURRENT_LIST_DIR}/py32f071xb.ld)
 function(update_board TARGET)
   target_compile_definitions(${TARGET} PUBLIC
     PY32F071xB
+    CFG_EXAMPLE_MSC_DUAL_READONLY
     CFG_EXAMPLE_VIDEO_READONLY
     )
 endfunction()
